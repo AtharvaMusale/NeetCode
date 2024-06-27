86aebe5da8c47c11d49550af5ed592525ba74209
AtharvaMusale <46114095+AtharvaMusale@users.noreply.github.com>
2024-06-27 08:12:47 -0400

    Time: 34 ms (74.24%) | Memory: 16.5 MB (73.96%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.py
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.py
@@ -1,13 +1,34 @@
 class Solution:
     def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
-        res = {}
-        if not root: return res        
-        def dfs(node,level):
-            if level not in res:
-                res[level] = node.val
-            if node.right:
-                dfs(node.right,level+1)
-            if node.left:
-                dfs(node.left,level+1)        
-        dfs(root,0)        
-        return res.values()
+        if root is None:
+            return []
+        
+        next_level = deque([root])
+        rightside =[]
+        while next_level:
+            curr_level = next_level
+            next_level = deque()
+
+            while curr_level:
+                node = curr_level.popleft()
+                if node.left:
+                    next_level.append(node.left)
+                
+                if node.right:
+                    next_level.append(node.right)
+                
+            rightside.append(node.val)
+        
+        return rightside
+
+        # res = {}
+        # if not root: return res        
+        # def dfs(node,level):
+        #     if level not in res:
+        #         res[level] = node.val
+        #     if node.right:
+        #         dfs(node.right,level+1)
+        #     if node.left:
+        #         dfs(node.left,level+1)        
+        # dfs(root,0)        
+        # return res.values()
