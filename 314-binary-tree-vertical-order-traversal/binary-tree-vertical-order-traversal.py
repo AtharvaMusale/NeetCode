e59617392d7db7533f5340055d376cb7413b2c6f
AtharvaMusale <46114095+AtharvaMusale@users.noreply.github.com>
2024-06-27 06:37:48 -0400

    Time: 34 ms (78.33%) | Memory: 16.5 MB (77.69%) - LeetSync

--- 314-binary-tree-vertical-order-traversal/binary-tree-vertical-order-traversal.py
+++ 314-binary-tree-vertical-order-traversal/binary-tree-vertical-order-traversal.py
@@ -5,20 +5,28 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def verticalOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+    def verticalOrder(self, root):
+        cols = collections.defaultdict(list)
+        queue = [(root, 0)]
+        for node, i in queue:
+            if node:
+                cols[i].append(node.val)
+                queue += (node.left, i - 1), (node.right, i + 1)
+        return [cols[i] for i in sorted(cols)]
+    # def verticalOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
 
-        hashmap = collections.defaultdict(list)
-        queue = deque([(root,0)])
+    #     hashmap = collections.defaultdict(list)
+    #     queue = deque([(root,0)])
 
-        while queue:
-            node, col = queue.popleft()
+    #     while queue:
+    #         node, col = queue.popleft()
 
-            if node is not None:
-                hashmap[col].append(node.val)
-                queue.append((node.left,col-1))
-                queue.append((node.right,col+1))
+    #         if node is not None:
+    #             hashmap[col].append(node.val)
+    #             queue.append((node.left,col-1))
+    #             queue.append((node.right,col+1))
         
-        return [hashmap[x] for x in sorted(hashmap.keys())]
+    #     return [hashmap[x] for x in sorted(hashmap.keys())]
         
             
 
