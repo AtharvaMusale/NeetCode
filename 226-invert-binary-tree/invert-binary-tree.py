de4c2851eb06c2f0511fe52059de4d9dceebeca7
AtharvaMusale <46114095+AtharvaMusale@users.noreply.github.com>
2024-06-28 04:11:06 -0400

    Time: 43 ms (8.59%) | Memory: 16.5 MB (65.09%) - LeetSync

--- 226-invert-binary-tree/invert-binary-tree.py
+++ 226-invert-binary-tree/invert-binary-tree.py
@@ -7,63 +7,15 @@
 
 class Solution:
     def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
-        if not root:
+ 
+        if root == None:
             return None
         
-        queue = collections.deque([root])
-        while queue:
-            current = queue.popleft()
-            current.left, current.right = current.right, current.left
-            
-            if current.left:
-                queue.append(current.left)
-            
-            if current.right:
-                queue.append(current.right)
-        
-        return root
-        
-# class Solution:
-#     def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
-#         if not root:
-#             return None
-        
-#         queue = collections.deque([root])
-#         while queue:
-#             current = queue.popleft()
-
-#             current.left, current.right = current.right, current.left
-
-#             while current.left:
-#                 queue.append(current.left)
-            
-#             while current.right:
-#                 queue.append(current.right)
-            
-        
-#         return root
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        # if root == None:
-        #     return None
-        
     
-        # root.left, root.right = root.right, root.left
+        root.left, root.right = root.right, root.left
 
-        # self.invertTree(root.left)
-        # self.invertTree(root.right)
-        # return root
+        self.invertTree(root.left)
+        self.invertTree(root.right)
+        return root
 
     
