e4956b55b361fe728ac0aece08262b58e89efd88
AtharvaMusale <46114095+AtharvaMusale@users.noreply.github.com>
2024-06-28 04:25:21 -0400

    Time: 42 ms (44.21%) | Memory: 17.5 MB (92.35%) - LeetSync

--- 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.py
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.py
@@ -4,6 +4,30 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
+# Using Iterative Stack Solution
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        stack = []
+        
+        if root:
+            stack.append([1,root])
+
+        depth = 0
+        while stack != []:
+            current_depth, root = stack.pop()
+            if root is not None:
+                depth = max(current_depth,depth)
+                stack.append((current_depth+1,  root.left))
+                stack.append((current_depth+1, root.right))
+
+        return depth
+
+
+
+
+# Recursive
+
 # class Solution:
 #     def maxDepth(self, root: Optional[TreeNode]) -> int:
 #         if not root:
@@ -13,18 +37,3 @@
 #         return 1 + max(lh,rh)
 
 
-class Solution:
-    def maxDepth(self, root: TreeNode) -> int:
-        stack = []
-        if root is not None:
-            stack.append((1, root))
-
-        depth = 0
-        while stack != []:
-            current_depth, root = stack.pop()
-            if root is not None:
-                depth = max(depth, current_depth)
-                stack.append((current_depth + 1, root.left))
-                stack.append((current_depth + 1, root.right))
-
-        return depth
