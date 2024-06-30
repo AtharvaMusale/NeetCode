c700828b043791382a92295daddeed6e7af530ea
AtharvaMusale <46114095+AtharvaMusale@users.noreply.github.com>
2024-06-30 19:59:47 -0400

    Time: 416 ms (55.85%) | Memory: 31.8 MB (76.68%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.py
+++ 217-contains-duplicate/contains-duplicate.py
@@ -1,15 +1,6 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        return len(list(set(nums)))!=len(nums)
+        hashset = set(nums)
+        return len(nums)!=len(list(hashset))
 
-
-# # Time complexity: O(n)
-# # Space complexity: O(n)
-# class Solution(object):
-#     def containsDuplicate(self, nums):
-#         hset = set()
-#         for idx in nums:
-#             if idx in hset:
-#                 return True
-#             else:
-#                 hset.add(idx)
+        
