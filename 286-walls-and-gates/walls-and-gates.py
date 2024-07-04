8201adb158bbc71cabd3cbcbadf2d877627336d5
AtharvaMusale <46114095+AtharvaMusale@users.noreply.github.com>
2024-07-04 13:39:07 -0400

    Time: 195 ms (48.95%) | Memory: 23.3 MB (20.98%) - LeetSync

---
+++ 286-walls-and-gates/walls-and-gates.py
@@ -0,0 +1,69 @@
+from collections import deque
+class Solution:
+    def wallsAndGates(self, rooms: List[List[int]]) -> None:
+        """
+        Do not return anything, modify rooms in-place instead.
+        """
+        rows, cols = len(rooms), len(rooms[0])
+        q = deque()
+        visit = set()
+
+        def addRooms(r,c):
+            if r<0 or r==rows or c<0 or c==cols or (r,c) in visit or rooms[r][c] == -1:
+                return
+            
+            visit.add((r,c))
+            q.append([r,c])
+
+        for i in range(rows):
+            for j in range(cols):
+                if rooms[i][j] == 0:
+                    q.append([i,j])
+                    visit.add((i,j))
+            
+        dist = 0
+        while q:
+            for i in range(len(q)):
+                r,c = q.popleft()
+                rooms[r][c] = dist
+                addRooms(r+1,c)
+                addRooms(r-1,c)
+                addRooms(r,c-1)
+                addRooms(r,c+1)
+            dist+=1
+
+
+# from collections import deque
+# class Solution:
+#     def wallsAndGates(self, rooms: List[List[int]]) -> None:
+#         """
+#         Do not return anything, modify rooms in-place instead.
+#         """
+#         rows, cols = len(rooms), len(rooms[0])
+#         q = deque()
+#         visit = set()
+
+#         def addRoom(r,c):
+#             if r<0 or r==rows or c<0 or c==cols or (r,c) in visit or rooms[r][c]==-1:
+#                 return 
+            
+#             visit.add((r,c))
+#             q.append([r,c])
+
+#         for i in range(rows):
+#             for j in range(cols):
+#                 if rooms[i][j] == 0:
+#                     q.append([i,j])
+#                     visit.add((i,j))
+
+
+#         dist = 0
+#         while q:
+#             for i in range(len(q)):
+#                 r,c = q.popleft()
+#                 rooms[r][c] = dist
+#                 addRoom(r+1,c)
+#                 addRoom(r-1,c)
+#                 addRoom(r,c+1)
+#                 addRoom(r,c-1)
+#             dist += 1
