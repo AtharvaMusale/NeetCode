332a494311c469e8179cbc14bbe0c16d58066fd2
AtharvaMusale <46114095+AtharvaMusale@users.noreply.github.com>
2024-07-09 17:51:18 -0400

    Time: 252 ms (47.31%) | Memory: 27.2 MB (12.03%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,15 +1,36 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        if len(grid) ==0:
-            return 0 
+#         if len(grid) ==0:
+#             return 0 
+#         vis = set()
+#         island = 0
+#         def dfs(i,j):
+#             for (x,y) in [(1,0),(0,1),(-1,0),(0,-1)]:
+#                 xi, yj = x + i, y + j
+#                 if 0<=xi<len(grid) and 0<=yj<len(grid[0]) and grid[i][j] == "1" and (xi,yj) not in vis:
+#                     vis.add((xi,yj))
+#                     dfs(xi,yj)
+        
+#         for i in range(len(grid)):
+#             for j in range(len(grid[0])):
+#                 if grid[i][j] == "1" and (i,j) not in vis:
+#                     island+=1
+#                     vis.add((i,j))
+#                     dfs(i,j)
+#         return island
+
         vis = set()
         island = 0
+
         def dfs(i,j):
             for (x,y) in [(1,0),(0,1),(-1,0),(0,-1)]:
-                xi, yj = x + i, y + j
-                if 0<=xi<len(grid) and 0<=yj<len(grid[0]) and grid[i][j] == "1" and (xi,yj) not in vis:
-                    vis.add((xi,yj))
-                    dfs(xi,yj)
+                xi , yi = i + x, j + y
+                if (0<=xi<len(grid)) and (0<=yi<len(grid[0])) and (grid[xi][yi] == "1") and (xi,yi) not in vis:
+                
+                    vis.add((xi,yi))
+                    dfs(xi,yi)
+
+            
         
         for i in range(len(grid)):
             for j in range(len(grid[0])):
@@ -17,29 +38,8 @@ class Solution:
                     island+=1
                     vis.add((i,j))
                     dfs(i,j)
+        
         return island
-
-        # vis = set()
-        # island = 0
-
-        # def dfs(i,j):
-        #     for (x,y) in [(1,0),(0,1),(-1,0),(0,-1)]:
-        #         xi , yi = i + x, j + y
-        #         if (0<=xi<len(grid)) and (0<=yi<len(grid[0])) and (grid[xi][yi] == "1") and (xi,yi) not in vis:
-                
-        #             vis.add((xi,yi))
-        #             dfs(xi,yi)
-
-            
-        
-        # for i in range(len(grid)):
-        #     for j in range(len(grid[0])):
-        #         if grid[i][j] == "1" and (i,j) not in vis:
-        #             island+=1
-        #             vis.add((i,j))
-        #             dfs(i,j)
-        
-        # return island
 
 
 # class Solution:
