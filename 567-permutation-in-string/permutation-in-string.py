d27458be66828a751132ae0e981b9f9565a77b7e
AtharvaMusale <46114095+AtharvaMusale@users.noreply.github.com>
2024-07-10 19:00:39 -0400

    Time: 3360 ms (6.46%) | Memory: 16.8 MB (17.72%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.py
+++ 567-permutation-in-string/permutation-in-string.py
@@ -1,14 +1,43 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        def sort_string(s: str) -> str:
-            return ''.join(sorted(s))
-        
-        sorted_s1 = sort_string(s1)
+
+        def sort_string(s):
+            return "".join(sorted(s))
+
+        s = sort_string(s1)
         len_s1 = len(s1)
         len_s2 = len(s2)
-        
-        for i in range(len_s2 - len_s1 + 1):
-            if sort_string(s2[i:i + len_s1]) == sorted_s1:
+
+        for i in range(len_s2-len_s1+1):
+            if  sort_string(s2[i:i+len_s1]) == s:
                 return True
-        
+            # else:
+            #     return False
         return False
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # def sort_string(s: str) -> str:
+        #     return ''.join(sorted(s))
+        
+        # sorted_s1 = sort_string(s1)
+        # len_s1 = len(s1)
+        # len_s2 = len(s2)
+        
+        # for i in range(len_s2 - len_s1 + 1):
+        #     if sort_string(s2[i:i + len_s1]) == sorted_s1:
+        #         return True
+        
+        # return False
