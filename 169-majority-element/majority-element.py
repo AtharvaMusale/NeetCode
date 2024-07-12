364a75ec37e648f0dc46cd2aa787c032aaade253
AtharvaMusale <46114095+AtharvaMusale@users.noreply.github.com>
2024-07-12 11:01:36 -0400

    Time: 178 ms (26.33%) | Memory: 18.1 MB (14.78%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -3,13 +3,5 @@ from collections import Counter
 
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        # Count the frequency of each element in the list
         count = Counter(nums)
-        
-        # Calculate the required count threshold
-        req = len(nums) // 2
-        
-        # Find and return the majority element
-        for num in count:
-            if count[num] > req:
-                return num
+        return max(count.keys(),key = count.get)
