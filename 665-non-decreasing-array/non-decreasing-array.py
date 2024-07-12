bb9e838eac8285988162aa44a75860c14eecc2a4
AtharvaMusale <46114095+AtharvaMusale@users.noreply.github.com>
2024-07-12 16:30:30 -0400

    Time: 146 ms (67.59%) | Memory: 17.9 MB (9.99%) - LeetSync

--- 665-non-decreasing-array/non-decreasing-array.py
+++ 665-non-decreasing-array/non-decreasing-array.py
@@ -1,7 +1,6 @@
 class Solution:
     def checkPossibility(self, nums: List[int]) -> bool:
-# class Solution:
-#     def checkPossibility(self, nums: List[int]) -> bool:
+
         cnt = 0
         for i in range(len(nums) - 1):
             if nums[i] > nums[i + 1]:
