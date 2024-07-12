c4e9b061a8fc806fa970523a360331cdfbaead9f
AtharvaMusale <46114095+AtharvaMusale@users.noreply.github.com>
2024-07-12 11:03:59 -0400

    Time: 172 ms (50.31%) | Memory: 18 MB (14.78%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,7 +1,7 @@
-from typing import List
-from collections import Counter
-
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        count = Counter(nums)
-        return max(count.keys(),key = count.get)
+        req = len(nums)//2
+        cnt = Counter(nums)
+        most_cnt = cnt.most_common(1)
+        return most_cnt[0][0]
+        # return most_cnt>req
