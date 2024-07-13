afafd891246eb72e41c398128bfa3a04eeba87e6
AtharvaMusale <46114095+AtharvaMusale@users.noreply.github.com>
2024-07-13 12:55:16 -0400

    Time: 229 ms (39.46%) | Memory: 18.1 MB (65.60%) - LeetSync

--- 658-find-k-closest-elements/find-k-closest-elements.py
+++ 658-find-k-closest-elements/find-k-closest-elements.py
@@ -1,18 +1,29 @@
+# class Solution:
+#     def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
+        # heap = []
+        # heapify(heap)
+        # for i in arr:
+        #     heappush(heap,(abs(x-i),i))
+        
+        # res = []
+        # while k>0:
+        #     res.append(heappop(heap)[1])
+        #     k-=1
+        
+        # return sorted(res)
+
 class Solution:
     def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
-        heap = []
-        heapify(heap)
-        for i in arr:
-            heappush(heap,(abs(x-i),i))
-        
-        res = []
-        while k>0:
-            res.append(heappop(heap)[1])
-            k-=1
-        
-        return sorted(res)
+        # Sort using custom comparator
+        sorted_arr = sorted(arr, key = lambda num: abs(x - num))
 
-
+        # Only take k elements
+        result = []
+        for i in range(k):
+            result.append(sorted_arr[i])
+        
+        # Sort again to have output in ascending order
+        return sorted(result)
 
 
 
