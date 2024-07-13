80bc5e2ffd79b015fe42a0c57c5bd889224054d0
AtharvaMusale <46114095+AtharvaMusale@users.noreply.github.com>
2024-07-13 10:44:03 -0400

    Time: 38 ms (69.53%) | Memory: 16.6 MB (13.42%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.py
+++ 88-merge-sorted-array/merge-sorted-array.py
@@ -4,18 +4,33 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        
-        p1 , p2 , p = 0, 0, 0
-        numsarr1 = arr1[:m]
-        while p<m+n:
-            if p2>=n or (p1<m and numsarr1[p1]<=arr2[p2]):
-                arr1[p] = numsarr1[p1]
-                p1+=1
-                p+=1
+        last = m + n -1
+
+        while m > 0 and n > 0:
+            if arr1[m-1] > arr2[n-1]:
+                arr1[last] = arr1[m-1]
+                m -= 1
+            
             else:
-                arr1[p] = arr2[p2]
-                p2+=1
-                p+=1
+                arr1[last] = arr2[n-1]
+                n-=1
+            
+            last-=1
+        
+        while n>0:
+            arr1[last] = arr2[n-1]
+            n, last = n-1, last-1
+        # p1 , p2 , p = 0, 0, 0
+        # numsarr1 = arr1[:m]
+        # while p<m+n:
+        #     if p2>=n or (p1<m and numsarr1[p1]<=arr2[p2]):
+        #         arr1[p] = numsarr1[p1]
+        #         p1+=1
+        #         p+=1
+        #     else:
+        #         arr1[p] = arr2[p2]
+        #         p2+=1
+        #         p+=1
 
 
 
