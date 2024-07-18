0922eb5dcd8a728bda8185f626d9fe5bcb6a7ccd
AtharvaMusale <46114095+AtharvaMusale@users.noreply.github.com>
2024-07-18 11:36:01 -0400

    Time: 359 ms (79.07%) | Memory: 30.3 MB (59.23%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.py
+++ 41-first-missing-positive/first-missing-positive.py
@@ -1,19 +1,31 @@
 class Solution:
     def firstMissingPositive(self, nums):
         n = len(nums)
-        seen = [False] * (n + 1)  # Array for lookup
+        seen = [False] * (len(nums)+1)
 
-        # Mark the elements from nums in the lookup array
         for num in nums:
-            if 0 < num <= n:
+            if 0<= num <= n:
                 seen[num] = True
-
-        # Iterate through integers 1 to n
-        # return smallest missing positive integer
-        for i in range(1, n + 1):
+        
+        for i in range(1,n+1):
             if not seen[i]:
                 return i
+            
+        return n+1
+        # n = len(nums)
+        # seen = [False] * (n + 1)  # Array for lookup
 
-        # If seen contains all elements 1 to n
-        # the smallest missing positive number is n + 1
-        return n + 1
+        # # Mark the elements from nums in the lookup array
+        # for num in nums:
+        #     if 0 < num <= n:
+        #         seen[num] = True
+
+        # # Iterate through integers 1 to n
+        # # return smallest missing positive integer
+        # for i in range(1, n + 1):
+        #     if not seen[i]:
+        #         return i
+
+        # # If seen contains all elements 1 to n
+        # # the smallest missing positive number is n + 1
+        # return n + 1
