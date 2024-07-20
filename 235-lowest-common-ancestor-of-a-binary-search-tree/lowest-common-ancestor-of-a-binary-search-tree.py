d9ca9eb4d591bfc43cf0bfb3f34a2dc6b525956d
AtharvaMusale <46114095+AtharvaMusale@users.noreply.github.com>
2024-07-20 15:52:51 -0400

    Time: 56 ms (50.43%) | Memory: 20.3 MB (63.81%) - LeetSync

--- 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.py
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.py
@@ -7,37 +7,36 @@
 
 class Solution:
     def lowestCommonAncestor(self, root: TreeNode, p: TreeNode, q: TreeNode) -> TreeNode:
-        # def dfs(node):
-        #     if node:
-        #         if p.val < node.val and q.val < node.val:
-        #             return dfs(node.left)
-        #         elif p.val > node.val and q.val > node.val:
-        #             return dfs(node.right)
-        #         else:
-        #             return node
-        #     return None
-        # return dfs(root)
-
-        # curr = root
-        # while curr:
-            
-        #     if p.val<curr.val and q.val<curr.val:
-        #         curr = curr.left
-
-        #     elif p.val > curr.val and q.val > curr.val:
-        #         curr = curr.right
-            
-        #     else:
-        #         return curr
-
-
         curr = root
         while curr:
             if p.val<curr.val and q.val < curr.val:
-                curr = curr.left
-            
+                curr = curr.left  
             elif p.val>curr.val and q.val>curr.val:
                 curr = curr.right
-            
             else:
                 return curr
+
+
+            # def dfs(node):
+            #     if node:
+            #         if p.val < node.val and q.val < node.val:
+            #             return dfs(node.left)
+            #         elif p.val > node.val and q.val > node.val:
+            #             return dfs(node.right)
+            #         else:
+            #             return node
+            #     return None
+            # return dfs(root)
+
+            # curr = root
+            # while curr:
+                
+            #     if p.val<curr.val and q.val<curr.val:
+            #         curr = curr.left
+
+            #     elif p.val > curr.val and q.val > curr.val:
+            #         curr = curr.right
+                
+            #     else:
+            #         return curr
+
