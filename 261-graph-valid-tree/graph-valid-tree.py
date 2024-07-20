3f494daf867af391c6a9c8895e6cd045f243ee14
AtharvaMusale <46114095+AtharvaMusale@users.noreply.github.com>
2024-07-20 13:58:11 -0400

    Time: 83 ms (76.13%) | Memory: 18.7 MB (6.24%) - LeetSync

--- 261-graph-valid-tree/graph-valid-tree.py
+++ 261-graph-valid-tree/graph-valid-tree.py
@@ -1,32 +1,3 @@
-# class Solution:
-#     def validTree(self, n: int, edges: List[List[int]]) -> bool:
-#         if not n:
-#             return True
-        
-#         adj = {i:[] for i in range(n)}
-#         for n1, n2 in edges:
-#             adj[n1].append(n2)
-#             adj[n2].append(n1)
-        
-#         visit = set()
-
-#         def dfs(i, prev):
-#             for i in visit:
-#                 return False
-            
-#             visit.add(i)
-#             for j in adj[i]:
-#                 if j==prev:
-#                     continue
-                
-#                 if not dfs(j,i):
-#                     return False
-            
-#             return True
-        
-#         return dfs(0,-1) and n==len(visit)
-        
- 
 from typing import List
 
 class Solution:
@@ -34,26 +5,63 @@ class Solution:
         if not n:
             return True
         
-        # Construct the adjacency list
-        adj = {i: [] for i in range(n)}
-        for n1, n2 in edges:
+        adj = {i:[] for i in range(n)}
+        for n1,n2 in edges:
             adj[n1].append(n2)
             adj[n2].append(n1)
-        
+    
         visit = set()
 
-        def dfs(node, prev):
+        def dfs(node,prev):
             if node in visit:
                 return False
-            
             visit.add(node)
-            for neighbor in adj[node]:
-                if neighbor == prev:
+            for nei in adj[node]:
+                if nei ==prev:
                     continue
-                if not dfs(neighbor, node):
+                if not dfs(nei,node):
                     return False
-            
             return True
         
-        # Start DFS from node 0
-        return dfs(0, -1) and n == len(visit)
+        return dfs(0,-1) and n==len(visit)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # if not n:
+        #     return True
+        
+        # # Construct the adjacency list
+        # adj = {i: [] for i in range(n)}
+        # for n1, n2 in edges:
+        #     adj[n1].append(n2)
+        #     adj[n2].append(n1)
+        
+        # visit = set()
+
+        # def dfs(node, prev):
+        #     if node in visit:
+        #         return False
+            
+        #     visit.add(node)
+        #     for neighbor in adj[node]:
+        #         if neighbor == prev:
+        #             continue
+        #         if not dfs(neighbor, node):
+        #             return False
+            
+        #     return True
+        
+        # # Start DFS from node 0
+        # return dfs(0, -1) and n == len(visit)
