6cc7cc2f2806aed2654c6975215d6fd7ba1ba47c
AtharvaMusale <46114095+AtharvaMusale@users.noreply.github.com>
2024-07-23 00:33:41 -0400

    Time: 48 ms (39.28%) | Memory: 18.1 MB (19.01%) - LeetSync

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.py
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.py
@@ -4,21 +4,43 @@ class Solution:
         # If Buy -> i + 1
         # If Sell -> i + 2
 
-        dp = {}  # key=(i, buying) val=max_profit
-
-        def dfs(i, buying):
-            if i >= len(prices):
+        dp ={}
+        def dfs(i,buying):
+            if i>= len(prices):
                 return 0
-            if (i, buying) in dp:
-                return dp[(i, buying)]
-
-            cooldown = dfs(i + 1, buying)
+            
+            if (i,buying) in dp:
+                return dp[(i,buying)]
+            
+            cooldown = dfs(i+1,buying)
             if buying:
-                buy = dfs(i + 1, not buying) - prices[i]
-                dp[(i, buying)] = max(buy, cooldown)
+                buy = dfs(i+1, not buying) - prices[i]
+                dp[(i,buying)] = max(buy,cooldown)
             else:
-                sell = dfs(i + 2, not buying) + prices[i]
-                dp[(i, buying)] = max(sell, cooldown)
-            return dp[(i, buying)]
+                sell = dfs(i+2, not buying) +prices[i]
+                dp[(i,buying)] = max(sell,cooldown)
+            return dp[(i,buying)]
+        return dfs(0,True)
 
-        return dfs(0, True)
+
+
+
+
+        # dp = {}  # key=(i, buying) val=max_profit
+
+        # def dfs(i, buying):
+        #     if i >= len(prices):
+        #         return 0
+        #     if (i, buying) in dp:
+        #         return dp[(i, buying)]
+
+        #     cooldown = dfs(i + 1, buying)
+        #     if buying:
+        #         buy = dfs(i + 1, not buying) - prices[i]
+        #         dp[(i, buying)] = max(buy, cooldown)
+        #     else:
+        #         sell = dfs(i + 2, not buying) + prices[i]
+        #         dp[(i, buying)] = max(sell, cooldown)
+        #     return dp[(i, buying)]
+
+        # return dfs(0, True)
