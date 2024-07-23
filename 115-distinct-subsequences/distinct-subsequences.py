bde60c853946a2179651f1389b2f196f1a558ccd
AtharvaMusale <46114095+AtharvaMusale@users.noreply.github.com>
2024-07-23 17:37:21 -0400

    Time: 831 ms (5.01%) | Memory: 233.1 MB (5.06%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.py
+++ 115-distinct-subsequences/distinct-subsequences.py
@@ -17,28 +17,46 @@
 #             return dp[(i,j)]
 
 #         return dfs(0,0)
+# class Solution:
+#     def numDistinct(self, s: str, t: str) -> int:
+#         dp = {}
+
+#         def dfs(i, j):
+#             if j == len(t):
+#                 return 1
+#             if i == len(s):
+#                 return 0
+
+#             if (i, j) in dp:
+#                 return dp[(i, j)]
+
+#             if s[i] == t[j]:
+#                 dp[(i, j)] = dfs(i + 1, j + 1) + dfs(i + 1, j)
+#             else:
+#                 dp[(i, j)] = dfs(i + 1, j)
+            
+#             return dp[(i, j)]
+
+#         return dfs(0, 0)
+            
+
 class Solution:
     def numDistinct(self, s: str, t: str) -> int:
-        dp = {}
+        cache = {}
 
-        def dfs(i, j):
-            if j == len(t):
-                return 1
-            if i == len(s):
-                return 0
+        for i in range(len(s) + 1):
+            cache[(i, len(t))] = 1
+        for j in range(len(t)):
+            cache[(len(s), j)] = 0
 
-            if (i, j) in dp:
-                return dp[(i, j)]
+        for i in range(len(s) - 1, -1, -1):
+            for j in range(len(t) - 1, -1, -1):
+                if s[i] == t[j]:
+                    cache[(i, j)] = cache[(i + 1, j + 1)] + cache[(i + 1, j)]
+                else:
+                    cache[(i, j)] = cache[(i + 1, j)]
+        return cache[(0, 0)]
 
-            if s[i] == t[j]:
-                dp[(i, j)] = dfs(i + 1, j + 1) + dfs(i + 1, j)
-            else:
-                dp[(i, j)] = dfs(i + 1, j)
-            
-            return dp[(i, j)]
-
-        return dfs(0, 0)
-            
         # dp = {}
         # m, n = len(s), len(t)
         # def findString(i, j):
