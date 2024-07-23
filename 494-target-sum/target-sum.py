44a7d1f4e8d373d9f9809072a0787aac2aa4742a
AtharvaMusale <46114095+AtharvaMusale@users.noreply.github.com>
2024-07-23 15:43:56 -0400

    Time: 206 ms (56.85%) | Memory: 37.8 MB (28.05%) - LeetSync

--- 494-target-sum/target-sum.py
+++ 494-target-sum/target-sum.py
@@ -1,7 +1,6 @@
 class Solution:
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
         dp = {}
-
         def backtrack(i,total):
             if i == len(nums):
                 return 1 if total == target else 0
@@ -9,6 +8,7 @@ class Solution:
             if (i,total) in dp:
                 return dp[(i,total)]
             
-            dp[(i,total)] = backtrack(i+1,total+nums[i]) + backtrack(i+1,total-nums[i])
+            dp[(i,total)] = backtrack(i+1, total+nums[i]) + backtrack(i+1, total-nums[i])
             return dp[(i,total)]
+        
         return backtrack(0,0)
