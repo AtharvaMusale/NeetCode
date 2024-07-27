0be18887dedf6732a7ff0a776150519e03bff14b
AtharvaMusale <46114095+AtharvaMusale@users.noreply.github.com>
2024-07-27 14:51:49 -0400

    Time: 58 ms (64.40%) | Memory: 16.4 MB (76.61%) - LeetSync

--- 2756-buy-two-chocolates/buy-two-chocolates.py
+++ 2756-buy-two-chocolates/buy-two-chocolates.py
@@ -1,8 +1,16 @@
+# class Solution:
+#     def buyChoco(self, prices: List[int], money: int) -> int:
+#         prices.sort()
+#         if prices[0]+prices[1]>money:
+#             return money
+#         else:
+#             return money - (prices[0]+prices[1])
+        
 class Solution:
     def buyChoco(self, prices: List[int], money: int) -> int:
         prices.sort()
-        if prices[0]+prices[1]>money:
-            return money
-        else:
-            return money - (prices[0]+prices[1])
-        
+        min_cost = prices[0] + prices[1]
+
+        if min_cost <= money:
+            return money - min_cost
+        return money
