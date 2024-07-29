79aee52695a73a3de63f6047b584175d1ce2a490
AtharvaMusale <46114095+AtharvaMusale@users.noreply.github.com>
2024-07-29 13:45:07 -0400

    Time: 57 ms (37.27%) | Memory: 16.6 MB (63.79%) - LeetSync

--- 39-combination-sum/combination-sum.py
+++ 39-combination-sum/combination-sum.py
@@ -1,143 +1,24 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         res = []
-        curr = []
-        def dfs(i,curr,total):
-            if total == target:
-                res.append(curr.copy())
+        subset = [] 
+
+        def dfs(i,total):
+            if total ==target:
+                res.append(subset.copy())
                 return
             
-            if i >= len(candidates) or total>target:
-                return
-            
-            curr.append(candidates[i])
-            dfs(i, curr, total+candidates[i])
-            curr.pop()
-            dfs(i+1, curr, total)
-        dfs(0,curr,0)
-        return res
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        # res = []
-        # curr = []
-
-        # def dfs(i, curr, total):
-        #     if total ==target:
-        #         res.append(curr.copy())
-        #         return
-        #     if i>= len(candidates) or total>target:
-        #         return
+            if total>target or i>=len(candidates):
+                return 
                 
-        #     curr.append(candidates[i])
-        #     dfs(i, curr, total+candidates[i])
-        #     curr.pop()
-        #     dfs(i+1, curr, total)
-        # dfs(0,curr,0)
-        # return res
+            # Include the current candidate and stay at the current index
+            subset.append(candidates[i])
+            dfs(i, total + candidates[i])
+            subset.pop()
+
+            # Exclude the current candidate and move to the next index
+            dfs(i + 1, total)
 
 
-
-
-
-        # res = []
-        # curr = []
-
-        # def dfs(i,curr,total):
-        #     if total == target:
-        #         res.append(curr.copy())
-        #         return
-            
-        #     if i>=len(candidates) or total>target:
-        #         return
-            
-        #     curr.append(candidates[i])
-        #     dfs(i,curr,total+candidates[i])
-        #     curr.pop()
-        #     dfs(i+1,curr,total)
-        # dfs(0,curr,0)
-
-        # return res
-
-
-
-        # res = []
-
-        # def dfs(i, curr, total):
-        #     if total == target:
-        #         res.append(curr.copy())
-        #         return
-            
-        #     if i >=len(candidates) or total > target:
-        #         return
-            
-        #     curr.append(candidates[i])
-        #     dfs(i, curr, total+candidates[i])
-        #     curr.pop()
-        #     dfs(i+1, curr, total)
-        
-        # dfs(0,[],0)
-        # return res
-
-
-
-
-
-
-
-
-
-
-# class Solution:
-#     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-#         res = []
-
-#         def dfs(i,curr,total):
-#             if total == target:
-#                 res.append(curr.copy())
-#                 return 
-#             if i>=len(candidates) or total > target:
-#                 return 
- 
-#             curr.append(candidates[i])
-#             dfs(i,curr,total+candidates[i])
-#             curr.pop()
-#             dfs(i+1,curr,total)
-        
-#         dfs(0,[],0)
-#         return res
-
-
-
-        # n = len(candidates)
-        # self.ans = set()
-        # def backtrack(idx, target, curr):
-        #     if target == 0:
-        #         self.ans.add(tuple(curr))
-        #         return
-        #     if idx >= n:
-        #         return
-        #     if target < 0:
-        #         return 
-        #     pick = backtrack(idx, target-candidates[idx], curr+[candidates[idx]])
-        #     notPick = backtrack(idx+1, target, curr)
-        #     return
-        # backtrack(0, target, [])
-        # return self.ans
-    
-
-        
+        dfs(0,0)
+        return res
