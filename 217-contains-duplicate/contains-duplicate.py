96c0cff806c463259d4553d85942818828423842
AtharvaMusale <46114095+AtharvaMusale@users.noreply.github.com>
2024-09-06 15:33:41 -0400

    Time: 419 ms (56.68%) | Memory: 31.9 MB (48.91%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.py
+++ 217-contains-duplicate/contains-duplicate.py
@@ -1,18 +1,11 @@
 
-# class Solution:
-#     def containsDuplicate(self, nums: List[int]) -> bool:
-#         hashmap = set()
-#         self.nums = nums
-#         for i in nums:
-#             if i  in hashmap:
-#                 return True
-#             else:
-#                 hashmap.add(i)
-#         return False
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        nums.sort()  # Sort the list
-        for i in range(1, len(nums)):
-            if nums[i] == nums[i - 1]:  # Check consecutive elements
+        hashmap = set()
+        self.nums = nums
+        for i in nums:
+            if i  in hashmap:
                 return True
+            else:
+                hashmap.add(i)
         return False
