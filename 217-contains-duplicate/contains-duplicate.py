34693ecf5576a92622e606926a149efac91244ee
AtharvaMusale <46114095+AtharvaMusale@users.noreply.github.com>
2024-09-06 15:33:32 -0400

    Time: 481 ms (5.11%) | Memory: 28.3 MB (92.08%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.py
+++ 217-contains-duplicate/contains-duplicate.py
@@ -1,8 +1,18 @@
+
+# class Solution:
+#     def containsDuplicate(self, nums: List[int]) -> bool:
+#         hashmap = set()
+#         self.nums = nums
+#         for i in nums:
+#             if i  in hashmap:
+#                 return True
+#             else:
+#                 hashmap.add(i)
+#         return False
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        hashset = set()
-        for i in nums:
-            if i in hashset:
+        nums.sort()  # Sort the list
+        for i in range(1, len(nums)):
+            if nums[i] == nums[i - 1]:  # Check consecutive elements
                 return True
-            hashset.add(i)
         return False
