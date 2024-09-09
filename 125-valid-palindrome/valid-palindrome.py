769cc8514ce79c860657a298c429b410965bf8a2
AtharvaMusale <46114095+AtharvaMusale@users.noreply.github.com>
2024-09-09 07:56:08 -0400

    Time: 44 ms (62.70%) | Memory: 17 MB (83.55%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -1,29 +1,19 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-
-        s_filtered = "".join(filter(str.isalnum,s.lower()))
-        start = 0
-        end = len(s_filtered)-1
-        while start <= end:
-            if s_filtered[start] != s_filtered[end]:
+        l, r = 0, len(s) - 1
+        
+        while l <= r:
+            # Skip non-alphanumeric characters for the left pointer
+            if not s[l].isalnum():
+                l += 1
+            # Skip non-alphanumeric characters for the right pointer
+            elif not s[r].isalnum():
+                r -= 1
+            # Compare the characters (ignore case)
+            elif s[l].lower() != s[r].lower():
                 return False
-            start+=1
-            end -= 1
-        
+            else:
+                l += 1
+                r -= 1
+                
         return True
-
-
-
-
-
-        # s_filtered = ''.join(filter(str.isalnum, s.lower()))
-        # start = 0
-        # end = len(s_filtered) - 1
-        
-        # while start <= end:
-        #     if s_filtered[start] != s_filtered[end]:
-        #         return False
-        #     start += 1
-        #     end -= 1
-        
-        # return True
