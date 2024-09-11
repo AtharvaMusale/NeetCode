8f60e1d2e5390ec247b64dcfbaf8525f91352c6c
AtharvaMusale <46114095+AtharvaMusale@users.noreply.github.com>
2024-09-11 11:36:26 -0400

    Time: 87 ms (70.04%) | Memory: 19.9 MB (66.85%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -9,23 +9,12 @@
             
 #             res[tuple(count)].append(s)
 #         return res.values()
-            
-from collections import defaultdict
-
 class Solution:
-    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        res = defaultdict(list)
-        
-        # Loop through each string in the list
-        for s in strs:
-            # Create a character count tuple as a hashable key
-            count = [0] * 26  # Initialize count for each letter in the alphabet
-            for c in s:
-                count[ord(c) - ord('a')] += 1  # Increment the count for the current character
-            
-            # Append the string to the corresponding list in the dictionary
-            res[tuple(count)].append(s)
-        
-        # Return the values as a list
-        return list(res.values())  # More explicit conversion to list for the return value
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:    
+        ans = defaultdict(list)
 
+        for s in strs:
+            key = "".join(sorted(s))
+            ans[key].append(s)
+        
+        return ans.values()
