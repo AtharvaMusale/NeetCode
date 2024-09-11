03d55fd4c1f65014bea1e017ec76e2180539d3a7
AtharvaMusale <46114095+AtharvaMusale@users.noreply.github.com>
2024-09-11 13:10:05 -0400

    Time: 317 ms (99.34%) | Memory: 30.8 MB (98.89%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.py
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.py
@@ -1,12 +1,45 @@
+# class Solution:
+#     def longestConsecutive(self, nums: List[int]) -> int:
+#         # hashset = set(nums)
+#         # longest = 0
+#         # for n in nums:
+#         #     if n-1 not in hashset:
+#         #         length = 1
+#         #         while (n+length) in hashset:
+#         #             length+=1
+                
+#         #         longest = max(longest,length)
+
+#         # return longest
+
+#         hashSet = set(nums)
+#         longest = 0
+#         for n in nums:
+#             # Check if (n-1) in hashSet
+#             if (n-1) not in hashSet:
+#                 length = 1
+#                 while (n+length) in hashSet:
+#                     length += 1
+#                 longest = max(length, longest)
+#         return longest
+
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
-        hashSet = set(nums)
-        longest = 0
-        for n in nums:
-            # Check if (n-1) in hashSet
-            if (n-1) not in hashSet:
-                length = 0
-                while (n+length) in hashSet:
-                    length += 1
-                longest = max(length, longest)
-        return longest
+        if len(nums) == 0:
+            return 0
+        nums.sort()
+        prev_num = nums[0]
+        max_size = 1
+        current_size = 1
+        for i in nums:
+            if (prev_num+1) == i:
+                current_size +=1 
+                if current_size > max_size:
+                    max_size = current_size
+            elif prev_num == i:
+                pass
+            else:
+                current_size = 1
+            prev_num = i
+        return max_size
+        
