ab024f2ba70f6ea862eee5845e83c0e782c63ee0
AtharvaMusale <46114095+AtharvaMusale@users.noreply.github.com>
2024-09-11 20:01:21 -0400

    Time: 556 ms (6.18%) | Memory: 29.9 MB (25.09%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.py
+++ 11-container-with-most-water/container-with-most-water.py
@@ -1,39 +1,14 @@
 class Solution:
-    def maxArea(self, arr: List[int]) -> int:
-        l = 0
-        r = len(arr)-1
-        area = 0
+    def maxArea(self, nums: List[int]) -> int:
+        l, r  = 0, len(nums)-1
+
+        total_area = 0
+
         while l<r:
-            area = max(area, (r-l) * (min(arr[l], arr[r])))
-            if arr[l] < arr[r]:
+            area = min(nums[l],nums[r]) * (r-l)
+            total_area = max(total_area,area)
+            if nums[l]<nums[r]:
                 l+=1
-            else:# arr[l]>=arr[r]:
+            else:
                 r-=1
-        
-        return area
-
-
-
-        # left, right = 0, len(arr)-1
-        # res = 0
-        
-        # while left<right:
-        #     res = max(res, (right-left) * min(arr[left],arr[right]))
-        #     if arr[left]<arr[right]:
-        #         left += 1       
-        #     elif arr[left] >= arr[right]:
-        #         right -= 1
-
-        # return res
-
-        # left , right = 0, len(arr)-1
-        # result = 0
-
-        # while left<right:
-        #     result = max(result,(right-left) * min(arr[left],arr[right]))
-        #     if arr[left]<arr[right]:
-        #         left+=1
-        #     elif arr[right]<=arr[left]:
-        #         right-=1
-        
-        # return result
+        return total_area
