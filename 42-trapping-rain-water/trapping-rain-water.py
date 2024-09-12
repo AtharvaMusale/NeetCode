724712d840552479b71065b6470389499296f75e
AtharvaMusale <46114095+AtharvaMusale@users.noreply.github.com>
2024-09-11 20:22:20 -0400

    Time: 117 ms (45.59%) | Memory: 18.3 MB (81.99%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.py
+++ 42-trapping-rain-water/trapping-rain-water.py
@@ -1,43 +1,18 @@
 class Solution:
-    def trap(self, height: List[int]) -> int:
-        ans = 0
-        stack = []
+    def trap(self, nums: List[int]) -> int:
+        res = 0
+        l, r = 0, len(nums)-1
+        leftMax, rightMax = nums[l], nums[r]
 
-        for i in range(len(height)):
-            while len(stack)>0 and height[i] > height[stack[-1]]:
-                top = stack.pop()
-                if len(stack) == 0:
-                    break
-                dis = i-stack[-1]-1
-                ht = min(height[i],height[stack[-1]]) - height[top]
-                ans+=dis * ht
-            stack.append(i)
-        
-        return ans
-                
+        while l<r:
+            if leftMax<rightMax:
+                l+=1
+                leftMax = max(leftMax,nums[l])
+                res += leftMax- nums[l]
 
+            else:
+                r-=1
+                rightMax = max(rightMax, nums[r])
+                res += rightMax- nums[r]
 
-
-
-
-
-
-
-
-
-
-
-
-        # ans =0
-        # st=[]
-        # for i in range(len(height)):
-        #     while(len(st) > 0 and height[i] > height[st[-1]]):
-        #         top = st.pop()
-        #         if len(st) ==0:
-        #             break
-        #         dis = i-st[-1] -1
-        #         ht = min(height[i], height[st[-1]]) - height[top]
-        #         ans+= dis*ht
-        #     st.append(i)
-        # return ans
-        
+        return res 
