7cf8b09f5499415c4823ffd0697442828b56d085
AtharvaMusale <46114095+AtharvaMusale@users.noreply.github.com>
2024-09-13 06:10:44 -0400

    Time: 52 ms (14.43%) | Memory: 17.1 MB (20.26%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.py
+++ 74-search-a-2d-matrix/search-a-2d-matrix.py
@@ -1,37 +1,34 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        m, n = len(matrix),  len(matrix[0])
-        l, r = 0, m*n -1
+        ROW, COL = len(matrix), len(matrix[0])
+        top, bot = 0, ROW-1
+        while top <= bot:
+            row = (top + bot) // 2
+            if target > matrix[row][-1]:
+                top = row + 1
 
-        while l<=r:
-            mid = (l+r)//2
-            row, col = mid // n, mid % n
-            
-            if matrix[row][col] == target:
-                return True
-            
-            elif target < matrix[row][col]:
-                r = mid-1
+            elif target < matrix[row][0]:
+                bot = row - 1
             
             else:
-                l = mid+1
+                break
             
+        
+        if not (top <= bot):
+            return False
+        
+
+        row = (top + bot)//2
+        l, r = 0 , COL-1
+        while l<=r:
+            mid = (l+r)//2
+            if target > matrix[row][mid]:
+                l = mid +1
+            
+            elif target < matrix[row][mid]:
+                r = mid - 1
+            
+            else:
+                return True
+        
         return False
-
-
-        # m, n = len(matrix), len(matrix[0])
-
-        # lo , hi = 0, m*n -1
-
-        # while lo <= hi:
-        #     mid = lo+(hi-lo)//2
-        #     row, col = mid//n, mid%n
-            
-        #     if matrix[row][col] == target:
-        #         return True
-        #     elif  target < matrix[row][col]:
-        #         hi = mid-1
-        #     else:
-        #         lo = mid +1
-        # return False
-
