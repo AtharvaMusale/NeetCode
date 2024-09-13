f048b771fbf3e2692f8526f56428fa6379e3d3ba
AtharvaMusale <46114095+AtharvaMusale@users.noreply.github.com>
2024-09-13 06:12:31 -0400

    Time: 50 ms (24.84%) | Memory: 17.1 MB (64.37%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.py
+++ 74-search-a-2d-matrix/search-a-2d-matrix.py
@@ -1,34 +1,54 @@
+# class Solution:
+#     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+#         ROW, COL = len(matrix), len(matrix[0])
+#         top, bot = 0, ROW-1
+#         while top <= bot:
+#             row = (top + bot) // 2
+#             if target > matrix[row][-1]:
+#                 top = row + 1
+
+#             elif target < matrix[row][0]:
+#                 bot = row - 1
+            
+#             else:
+#                 break
+            
+        
+#         if not (top <= bot):
+#             return False
+        
+
+#         row = (top + bot)//2
+#         l, r = 0 , COL-1
+#         while l<=r:
+#             mid = (l+r)//2
+#             if target > matrix[row][mid]:
+#                 l = mid +1
+            
+#             elif target < matrix[row][mid]:
+#                 r = mid - 1
+            
+#             else:
+#                 return True
+        
+#         return False
+
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        ROW, COL = len(matrix), len(matrix[0])
-        top, bot = 0, ROW-1
-        while top <= bot:
-            row = (top + bot) // 2
-            if target > matrix[row][-1]:
-                top = row + 1
+        ROWS, COLS = len(matrix), len(matrix[0])
 
-            elif target < matrix[row][0]:
-                bot = row - 1
-            
-            else:
-                break
-            
-        
-        if not (top <= bot):
-            return False
-        
+        # Binary search in a "virtual" 1D list of the matrix
+        left, right = 0, ROWS * COLS - 1
 
-        row = (top + bot)//2
-        l, r = 0 , COL-1
-        while l<=r:
-            mid = (l+r)//2
-            if target > matrix[row][mid]:
-                l = mid +1
-            
-            elif target < matrix[row][mid]:
-                r = mid - 1
-            
-            else:
+        while left <= right:
+            mid = (left + right) // 2
+            mid_value = matrix[mid // COLS][mid % COLS]  # Map mid to the actual matrix element
+
+            if mid_value == target:
                 return True
-        
+            elif mid_value < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+
         return False
