87ff82652088a970e4108d6eb175f431053880aa
AtharvaMusale <46114095+AtharvaMusale@users.noreply.github.com>
2024-09-14 14:45:35 -0400

    Time: 26 ms (98.12%) | Memory: 17.6 MB (85.29%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.py
+++ 206-reverse-linked-list/reverse-linked-list.py
@@ -3,17 +3,34 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+# class Solution:
+#     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+#         # Iterative solution
+#         prev = None
+#         curr = head
+
+#         while curr:
+#             temp = curr.next
+#             curr.next = prev
+#             prev = curr
+#             curr = temp
+        
+#         return prev
+
+
+# Recursive Solution
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        prev, curr = None, head
+        if not head:
+            return None
+        
+        newHead = head
 
-        # while not head:
-        #     return None
+        if head.next:
+            newHead = self.reverseList(head.next)
+            head.next.next = head
+        head.next = None
+    
+        return newHead
+
         
-        while curr:
-            tmp = curr.next
-            curr.next = prev
-            prev = curr
-            curr = tmp
-        
-        return prev
