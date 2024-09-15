bc01df95cfcae2ce5e2b6f5e80dc4f5443909168
AtharvaMusale <46114095+AtharvaMusale@users.noreply.github.com>
2024-09-15 13:22:18 -0400

    Time: 33 ms (91.70%) | Memory: 17.6 MB (42.73%) - LeetSync

--- 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.py
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.py
@@ -4,85 +4,33 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-
-# Using Iterative Stack Solution
-class Solution:
-    def maxDepth(self, root: Optional[TreeNode]) -> int:
-        # if not root:
-        #     return 0
-        
-        # lh = self.maxDepth(root.left)
-        # rh = self.maxDepth(root.right)
-
-        # return 1 + max(lh,rh)
-    
-        # stack = []
-        # if root is not None:
-        #     stack.append((1,root))
-        
-        # depth = 0
-        # while stack != []:
-        #     curr_depth, root = stack.pop()
-        #     if root is not None:
-        #         depth = max(depth,curr_depth)
-        #         stack.append((curr_depth+1, root.left))
-        #         stack.append((curr_depth+1, root.right))
-        
-        # return depth
-
-
-
-        stack = []
-        if root:
-       
-            stack.append((1,root))
-        depth = 0
-        curr_depth = 0
-        while stack:
-            curr_depth, root = stack.pop()
-            if root:
-                depth = max(depth, curr_depth)
-                stack.append((curr_depth+1, root.left))
-                stack.append((curr_depth+1, root.right))
-            
-        return depth
-
-
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        # stack = []
-        
-        # if root:
-        #     stack.append([1,root])
-
-        # depth = 0
-        # while stack != []:
-        #     current_depth, root = stack.pop()
-        #     if root is not None:
-        #         depth = max(current_depth,depth)
-        #         stack.append((current_depth+1,  root.left))
-        #         stack.append((current_depth+1, root.right))
-
-        # return depth
-
-
-
-
-# Recursive
-
 # class Solution:
 #     def maxDepth(self, root: Optional[TreeNode]) -> int:
 #         if not root:
 #             return 0
-#         lh = self.maxDepth(root.left)
-#         rh = self.maxDepth(root.right)
-#         return 1 + max(lh,rh)
+        
+#         left = self.maxDepth(root.left)
+#         right = self.maxDepth(root.right)
+#         return 1+ max(left,right)
 
 
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+        
+        level = 0
+        q = deque([root])
+
+        while q:
+            for i in range(len(q)):
+                node = q.popleft()
+                if node.left:
+                    q.append(node.left)
+                
+                if node.right:
+                    q.append(node.right)
+                
+            level+=1
+
+        return level
