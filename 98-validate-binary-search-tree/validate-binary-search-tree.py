3d2a21c3e435309fe1eb87270f4e3a234c3433a9
AtharvaMusale <46114095+AtharvaMusale@users.noreply.github.com>
2024-09-17 17:23:38 -0400

    Time: 50 ms (8.14%) | Memory: 18.4 MB (62.03%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.py
+++ 98-validate-binary-search-tree/validate-binary-search-tree.py
@@ -4,42 +4,14 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-
-# class Solution:
-#     def isValidBST(self, root: Optional[TreeNode]) -> bool:
-#         def validate(node,low = math.inf,high = math.inf):
-#             if not node:
-#                 return True
-            
-#             if node.val<=low or node.val >= high:
-#                 return False
-            
-#             return validate(node.right,node.val,high) and validate(node.left,low,node.val)
-#         return validate(root)
-
-        # def isValid(node):
-        #     if node is None:
-        #         return True, float("inf"), -float("inf")
-            
-        #     lbst, lmin, lmax = isValid(node.left)
-        #     rbst, rmin, rmax = isValid(node.right)
-
-        #     if lbst and rbst and lmax<node.val<rmin:
-        #         return True, min(lmin,node.val), max(rmax,node.val)
-        #     return False, 0, 0
-        # ans, _, _ = isValid(root)
-    
-        # return ans
-
 class Solution:
     def isValidBST(self, root: Optional[TreeNode]) -> bool:
-        def validate(node, low=-math.inf, high=math.inf):
+        def valid(node, left, right):
             if not node:
-                return True
-            
-            if node.val <= low or node.val >= high:
+                return  True
+            if not (left < node.val < right):
                 return False
             
-            return validate(node.right, node.val, high) and validate(node.left, low, node.val)
-        
-        return validate(root)
+            return valid(node.left, left, node.val) and valid(node.right, node.val, right)
+        return valid(root, float("-inf"), float("inf"))
+            
