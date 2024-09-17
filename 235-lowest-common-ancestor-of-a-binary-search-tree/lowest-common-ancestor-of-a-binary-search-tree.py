80d88e2876036ae9aaa8511404468f3597be05c4
AtharvaMusale <46114095+AtharvaMusale@users.noreply.github.com>
2024-09-17 12:41:34 -0400

    Time: 57 ms (45.24%) | Memory: 20.3 MB (32.40%) - LeetSync

--- 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.py
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.py
@@ -1,42 +1,18 @@
 # Definition for a binary tree node.
 # class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
 
 class Solution:
-    def lowestCommonAncestor(self, root: TreeNode, p: TreeNode, q: TreeNode) -> TreeNode:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
         curr = root
         while curr:
-            if p.val<curr.val and q.val < curr.val:
-                curr = curr.left  
+            if p.val<curr.val and q.val<curr.val:
+                curr = curr.left
             elif p.val>curr.val and q.val>curr.val:
                 curr = curr.right
             else:
                 return curr
-
-
-            # def dfs(node):
-            #     if node:
-            #         if p.val < node.val and q.val < node.val:
-            #             return dfs(node.left)
-            #         elif p.val > node.val and q.val > node.val:
-            #             return dfs(node.right)
-            #         else:
-            #             return node
-            #     return None
-            # return dfs(root)
-
-            # curr = root
-            # while curr:
                 
-            #     if p.val<curr.val and q.val<curr.val:
-            #         curr = curr.left
-
-            #     elif p.val > curr.val and q.val > curr.val:
-            #         curr = curr.right
-                
-            #     else:
-            #         return curr
-
