ccb6a5eaa02d62cbfa44945a3390737ebcd280ba
AtharvaMusale <46114095+AtharvaMusale@users.noreply.github.com>
2024-09-17 16:28:15 -0400

    Time: 39 ms (38.34%) | Memory: 16.5 MB (71.21%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.py
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.py
@@ -4,26 +4,51 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+# class Solution:
+#     def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+#         res = []
+#         if not root:
+#             return res
+
+#         q = collections.deque([root])
+
+#         while q:
+#             level = len(q)
+#             for i in range(level):
+#                 node = q.popleft()
+#                 if i == level - 1:
+#                     res.append(node.val)
+#                 if node.left:
+#                     q.append(node.left)
+#                 if node.right:
+#                     q.append(node.right)
+                
+#         return res
+
 class Solution:
     def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
         res = []
+        q = deque([root])
         if not root:
             return res
-
-        q = collections.deque([root])
 
         while q:
             level = len(q)
             for i in range(level):
                 node = q.popleft()
-                if i == level - 1:
+                if i == level-1:
                     res.append(node.val)
+                
                 if node.left:
                     q.append(node.left)
+                
                 if node.right:
                     q.append(node.right)
                 
         return res
+
+
+
 
 
 
