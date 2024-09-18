04a0616e112f7524a29d4b54f8cdf9d29f87b988
AtharvaMusale <46114095+AtharvaMusale@users.noreply.github.com>
2024-09-18 12:41:05 -0400

    Time: 32 ms (79.22%) | Memory: 16.3 MB (97.33%) - LeetSync

--- 1127-last-stone-weight/last-stone-weight.py
+++ 1127-last-stone-weight/last-stone-weight.py
@@ -3,11 +3,13 @@ class Solution:
         stones = [-s for s in stones]
         heapq.heapify(stones)
 
-        while len(stones)>1:
-            a = heapq.heappop(stones)
-            b = heapq.heappop(stones)
-            if b > a:
-                heapq.heappush(stones, a - b)
+        while len(stones) > 1:
+            first = heapq.heappop(stones)  # Pop the largest
+            second = heapq.heappop(stones)  # Pop the second largest
+            
+            if first != second:
+                # Push the difference as negative since we're simulating a max heap
+                heapq.heappush(stones, -abs(first - second))
 
         return abs(stones[0]) if stones else 0
             
