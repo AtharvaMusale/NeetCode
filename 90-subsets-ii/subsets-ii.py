0551a2bcc58fcd7f821fcfbb14fa683c6a9a2b1b
AtharvaMusale <46114095+AtharvaMusale@users.noreply.github.com>
2024-09-19 13:41:38 -0400

    Time: 41 ms (45.55%) | Memory: 16.7 MB (78.85%) - LeetSync

--- 90-subsets-ii/subsets-ii.py
+++ 90-subsets-ii/subsets-ii.py
@@ -3,16 +3,23 @@ class Solution:
         res = []
         subset = []
         nums.sort()
+
         def dfs(i,subset):
             if i == len(nums):
                 res.append(subset.copy())
                 return
-            
+            # Include the ith element
             subset.append(nums[i])
             dfs(i+1,subset)
             subset.pop()
+            #  Not include ith element
+
             while i+1 <len(nums) and nums[i] == nums[i+1]:
                 i+=1
+            
             dfs(i+1,subset)
         dfs(0,[])
         return res
+        
+
+            
