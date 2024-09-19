3105178c08b6109f819b138ba623b4fe62e87120
AtharvaMusale <46114095+AtharvaMusale@users.noreply.github.com>
2024-09-19 13:00:35 -0400

    Time: 58 ms (32.22%) | Memory: 16.8 MB (10.44%) - LeetSync

--- 39-combination-sum/combination-sum.py
+++ 39-combination-sum/combination-sum.py
@@ -1,46 +1,46 @@
-# class Solution:
-#     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-#         res = []
-#         subset = [] 
-
-#         def dfs(i,total):
-#             if total ==target:
-#                 res.append(subset.copy())
-#                 return
-            
-#             if total>target or i>=len(candidates):
-#                 return 
-
-#             # Include the current candidate and stay at the current index
-#             subset.append(candidates[i])
-#             dfs(i, total + candidates[i])
-#             subset.pop()
-
-#             # Exclude the current candidate and move to the next index
-#             dfs(i + 1, total)
-
-
-#         dfs(0,0)
-#         return res
-
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-       
         res = []
-        subset = []
+        subset = [] 
 
-        def dfs(i, total):
-            if total == target:
+        def dfs(i,total):
+            if total ==target:
                 res.append(subset.copy())
                 return
             
-            if i>= len(candidates) or total>target:
-                return
+            if total>target or i>=len(candidates):
+                return 
+
+            # Include the current candidate and stay at the current index
             subset.append(candidates[i])
-            dfs(i, total+ candidates[i])
+            dfs(i, total + candidates[i])
             subset.pop()
 
-            dfs(i+1, total)
-        
+            # Exclude the current candidate and move to the next index
+            dfs(i + 1, total)
+
+
         dfs(0,0)
         return res
+
+# class Solution:
+#     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+       
+#         res = []
+#         subset = []
+
+#         def dfs(i, total):
+#             if total == target:
+#                 res.append(subset.copy())
+#                 return
+            
+#             if i>= len(candidates) or total>target:
+#                 return
+#             subset.append(candidates[i])
+#             dfs(i, total+ candidates[i])
+#             subset.pop()
+
+#             dfs(i+1, total)
+        
+#         dfs(0,0)
+#         return res
