22f248ab832e215afff95320cfb05760ff1829cd
AtharvaMusale <46114095+AtharvaMusale@users.noreply.github.com>
2024-09-19 12:59:54 -0400

    Time: 57 ms (35.95%) | Memory: 16.8 MB (10.44%) - LeetSync

--- 39-combination-sum/combination-sum.py
+++ 39-combination-sum/combination-sum.py
@@ -25,19 +25,22 @@
 
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+       
         res = []
+        subset = []
 
-        def dfs(i, cur, total):
+        def dfs(i, total):
             if total == target:
-                res.append(cur.copy())
+                res.append(subset.copy())
                 return
-            if i >= len(candidates) or total > target:
+            
+            if i>= len(candidates) or total>target:
                 return
+            subset.append(candidates[i])
+            dfs(i, total+ candidates[i])
+            subset.pop()
 
-            cur.append(candidates[i])
-            dfs(i, cur, total + candidates[i])
-            cur.pop()
-            dfs(i + 1, cur, total)
-
-        dfs(0, [], 0)
+            dfs(i+1, total)
+        
+        dfs(0,0)
         return res
