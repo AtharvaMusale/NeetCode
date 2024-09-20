f84c7c010362f54b368c0e9f7c95b3edc536aa21
AtharvaMusale <46114095+AtharvaMusale@users.noreply.github.com>
2024-09-19 20:07:40 -0400

    Time: 3947 ms (66.63%) | Memory: 16.6 MB (69.06%) - LeetSync

--- 79-word-search/word-search.py
+++ 79-word-search/word-search.py
@@ -1,70 +1,27 @@
 class Solution:
-    def exist(self, board: List[List[str]], s: str) -> bool:
-        m, n = len(board), len(board[0])
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        ROWS, COLS = len(board), len(board[0])
+        
         path = set()
+
         def dfs(r, c, i):
-            if i == len(s):
-                return True
-            
-            if r<0 or c< 0 or r>=m or c>=n or s[i] != board[r][c] or (r,c) in path:
+            if i == len(word):
+                return word
+
+            if (r < 0 or c < 0 or r >= ROWS or c >= COLS or board[r][c]!= word[i] or (r,c) in path):
                 return False
             
             path.add((r,c))
-            res =  (dfs(r+1,c,i+1) or
-            dfs(r,c+1,i+1) or
-            dfs(r-1,c,i+1) or
-            dfs(r,c-1,i+1)) 
 
+            res = (dfs(r+1, c, i+1) or dfs(r-1, c, i+1) or dfs(r, c+1, i+1) or dfs(r, c-1, i+1))
             path.remove((r,c))
+
             return res
-        for r in range(m):
-            for c in range(n):
+        
+        for r in range(ROWS):
+            for c in range(COLS):
                 if dfs(r,c,0):
                     return True
-
-        return False
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        # m, n = len(board), len(board[0])
-   
-        # def find(sInd, i, j, vis):
-        #     if sInd == len(s)-1:
-        #         print(vis)
-        #         return True
-        #     vis.append((i, j))
-        #     ans = False
-        #     for nx, ny in [(i+1, j), (i-1, j), (i,j+1), (i, j-1)]:
-        #         if 0<=nx<m and 0<=ny<n and (nx, ny) not in vis and board[nx][ny] == s[sInd+1]:
-        #             ans = ans or find(sInd+1, nx, ny, vis)
-        #     vis.pop()
-        #     return ans
-        # for i in range(m):
-        #     for j in range(n):
-        #         if board[i][j] == s[0] and find(0, i, j,[] ):
-        #             return True
-        # return False
-
-   
-   
-   
-   
-    # if board[i][j] == word[0]
-    # if find(0,i, j)
         
+        return False
+            
