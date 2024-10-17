dfea813ea6ffc4d34037d317e0a3dcee36376bfc
AtharvaMusale <46114095+AtharvaMusale@users.noreply.github.com>
2024-10-17 13:47:21 -0400

    Time: 6 ms (100.00%) | Memory: 30.5 MB (91.27%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.py
+++ 217-contains-duplicate/contains-duplicate.py
@@ -1,9 +1,19 @@
 
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        hashmap = set()
-        for i in nums:
-            if i in hashmap:
-                return True
-            hashmap.add(i)
+        # Brute FOrce- O(n^2)
+        # Two loops and checking if the element is present or not
+        # for i in range(len(nums)):
+        #     for j in range(i+1,len(nums)):
+        #         if nums[i] == nums[j]:
+        #             return True
         
+        # return False
+
+        hashset = set()
+        for i in nums:
+            if i in hashset:
+                return True
+            hashset.add(i)
+        return False
+            
