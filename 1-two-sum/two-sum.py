023e0cb7624a6bad31b99d4ff24de3ca479be12e
AtharvaMusale <46114095+AtharvaMusale@users.noreply.github.com>
2024-10-17 14:28:09 -0400

    Time: 1 ms (100.00%) | Memory: 17.9 MB (12.50%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,10 +1,15 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        # Brute Force O(n^2)
+        # Hashmap
         hashmap = {}
-        # hasmap[Value] = index
+        # [2,7,11,15] # 9
+        # [2+7 = 9]
+        # The array is sorted or not?
+        # If Not then still O(n)
         for i,j in enumerate(nums):
             diff = target - j
             if diff in hashmap:
                 return [hashmap[diff], i]
             hashmap[j] = i
-        
+        return 
