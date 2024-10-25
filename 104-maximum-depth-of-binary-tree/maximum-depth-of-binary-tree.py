c80ce6746c94a7d0cc89a4565abd2d1a5340c41e
AtharvaMusale <46114095+AtharvaMusale@users.noreply.github.com>
2024-10-25 17:57:21 -0400

    Time: 0 ms (100.00%) | Memory: 17.9 MB (11.71%) - LeetSync

--- 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.py
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.py
@@ -4,71 +4,13 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-# class Solution:
-#     def maxDepth(self, root: Optional[TreeNode]) -> int:
-        # if not root:
-        #     return 0
-        
-        # left = self.maxDepth(root.left)
-        # right = self.maxDepth(root.right)
-        # height = 1 + max(left,right)
-
-        # return height
 class Solution:
     def maxDepth(self, root: Optional[TreeNode]) -> int:
         if not root:
             return 0
-
-        q = deque([root])
-        level = 0
-        while q:
-            for i in range(len(q)):
-                node = q.popleft()
-                if node.left:
-                    q.append(node.left)
-                
-                if node.right:
-                    q.append(node.right)
-                
-            level += 1
-            
-        return level
-
-
-# class Solution:
-#     def maxDepth(self, root: Optional[TreeNode]) -> int:
-#         if not root:
-#             return 0
         
-#         level = 0
-#         q = deque([root])
+        left = self.maxDepth(root.left)
+        right = self.maxDepth(root.right)
 
-#         while q:
-#             for i in range(len(q)):
-#                 node = q.popleft()
-#                 if node.left:
-#                     q.append(node.left)
-                
-#                 if node.right:
-#                     q.append(node.right)
-                
-#             level+=1
-
-#         return level
-
-# class Solution:
-#     def maxDepth(self, root: Optional[TreeNode]) -> int:
-
-
-#         stack = [[root,1]]
-#         res = 0
-
-#         while stack:
-#             node, depth = stack.pop()
-
-#             if node:
-#                 res = max(res, depth)
-#                 stack.append([node.left,depth+1])
-#                 stack.append([node.right,depth+1])
-
-#         return res
+        return 1 + max(left,right)
+        
