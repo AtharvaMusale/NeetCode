138d1dd00ca7137606bc872bf164094f67e51671
AtharvaMusale <46114095+AtharvaMusale@users.noreply.github.com>
2024-10-25 14:41:17 -0400

    Time: 251 ms (48.80%) | Memory: 27.9 MB (7.76%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,157 +1,26 @@
-# class Solution:
-#     def numIslands(self, grid: List[List[str]]) -> int:
-#         if len(grid) ==0:
-#             return 0 
-#         vis = set()
-#         island = 0
-#         def dfs(i,j):
-#             for (x,y) in [(1,0),(0,1),(-1,0),(0,-1)]:
-#                 xi, yj = x + i, y + j
-#                 if 0<=xi<len(grid) and 0<=yj<len(grid[0]) and grid[i][j] == "1" and (xi,yj) not in vis:
-#                     vis.add((xi,yj))
-#                     dfs(xi,yj)
-        
-#         for i in range(len(grid)):
-#             for j in range(len(grid[0])):
-#                 if grid[i][j] == "1" and (i,j) not in vis:
-#                     island+=1
-#                     vis.add((i,j))
-#                     dfs(i,j)
-#         return island
-
-        # vis = set()
-        # island = 0
-
-        # def dfs(i,j):
-        #     for (x,y) in [(1,0),(0,1),(-1,0),(0,-1)]:
-        #         xi , yi = i + x, j + y
-        #         if (0<=xi<len(grid)) and (0<=yi<len(grid[0])) and (grid[xi][yi] == "1") and (xi,yi) not in vis:
-                
-        #             vis.add((xi,yi))
-        #             dfs(xi,yi)
-
-            
-        
-        # for i in range(len(grid)):
-        #     for j in range(len(grid[0])):
-        #         if grid[i][j] == "1" and (i,j) not in vis:
-        #             island+=1
-        #             vis.add((i,j))
-        #             dfs(i,j)
-        
-        # return island
-
-
-# class Solution:
-#     def numIslands(self, grid: List[List[str]]) -> int:
-#         vis = set()
-#         cnt = 0
-
-#         def dfs(i,j):
-#             for (x,y) in [(1,0), (0,1), (-1,0), (0,-1)]:
-#                 xi, yj = x+i, y+j
-#                 if (0<=xi<len(grid) and 0<=yj<len(grid[0])) and grid[xi][yj] =="1" and (xi,yj) not in vis:
-#                     if (xi,yj) not in vis:
-#                         vis.add((xi,yj))
-#                         dfs(xi,yj)
-
-
-#         for i in range(len(grid)):
-#             for j in range(len(grid[0])):
-#                 if grid[i][j] == "1" and (i,j) not in vis:
-#                     cnt+=1
-#                     vis.add((i,j))
-#                     dfs(i,j)
-        
-#         return cnt
-
-
-
-
-
-        # vis = set()
-        # ones=[]
-        # cnt=0
-        # rows, cols = len(grid), len(grid[0])
-      
-        # def dfs(i,j):
-        #     for (x, y) in [(0,1), (1,0), (0, -1), (-1,0)]:
-        #         xi, yj =x+i, y+j
-        #         if (0<=xi<rows and 0<=yj<cols) and grid[xi][yj] == "1" and (xi,yj) not in vis:
-        #             vis.add((xi,yj))
-        #             dfs(xi,yj)
-        #     return
-
-
-            
-        # for i in range(len(grid)):
-        #      for j in range(len(grid[0])):
-        #          if grid[i][j] == "1" and (i,j) not in vis:
-        #             vis.add((i,j))
-        #             cnt+=1
-        #             dfs(i,j)
-        # return cnt
-        
-
-
-
-
-
-
-
-
-
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        if not grid or not grid[0]:
-            return 0
-        ROWS, COLS = len(grid), len(grid[0])
+        visit = set()
         island = 0
-        boundary = [[1,0],[0,1],[-1,0],[0,-1]]
-        def dfs(x,y):
+        ROWS, COLS = len(grid), len(grid[0])
 
-            if x<0 or y<0 or x>=ROWS or y>= COLS or grid[x][y]== "0":
-                return 
-
-            grid[x][y] = "0"
+        def dfs(r,c):
             
-            for i,j in boundary:
-                xi = x + i
-                yj = y +j
+            if r< 0 or r >= ROWS or c<0 or c >= COLS or (r,c) in visit or grid[r][c] == "0":
+                return 
+            
+            visit.add((r,c))
 
-                dfs(xi,yj)
-            pass
+            dfs(r+1,c) 
+            dfs(r-1,c)
+            dfs(r,c+1)
+            dfs(r,c-1)
+
+        for r in range(ROWS):
+            for c in range(COLS):
+                if grid[r][c] == "1" and (r,c) not in visit:
+                    island +=1
+                    dfs(r,c)
         
-        for i in range(ROWS):
-            for j in range(COLS):
-                if grid[i][j] == "1":
-                    island+=1
-                    dfs(i,j)
-                
         return island
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
