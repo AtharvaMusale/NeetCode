c22adb287b070241187a507ff5436372a0458c0e
AtharvaMusale <46114095+AtharvaMusale@users.noreply.github.com>
2024-10-25 17:23:06 -0400

    Time: 4 ms (96.57%) | Memory: 18.4 MB (65.05%) - LeetSync

--- 207-course-schedule/course-schedule.py
+++ 207-course-schedule/course-schedule.py
@@ -1,138 +1,34 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        preMap ={i:[] for i in range(numCourses)}
-        for crs,pre in prerequisites:
-            preMap[crs].append(pre)
+     
+
+        preMap = {i: [] for i in range(numCourses)}
+        visit = set()      
+        for crs, prq in prerequisites:
+            preMap[crs].append(prq)
         
-        visit = set()
         def dfs(crs):
+
             if crs in visit:
                 return False
-            
-            if preMap[crs] == []:
+
+            elif preMap[crs] == []:
                 return True
+
+            
             visit.add(crs)
+
             for pre in preMap[crs]:
                 if not dfs(pre):
                     return False
-            
+                
             visit.remove(crs)
             preMap[crs] = []
 
             return True
-
+        
         for crs in range(numCourses):
             if not dfs(crs):
                 return False
             
         return True
-
-
-
-
-
-
-
-
-        preMap = {i:[] for i in range(numCourses)}
-
-        for crs,pre in prerequisites:
-            preMap[crs].append(pre)
-        
-        visit = set()
-        def dfs(crs):
-            if crs in visit:
-                return False
-            
-            if preMap[crs] == []:
-                return True
-            
-            visit.add(crs)
-            for pre in preMap[crs]:
-                if not dfs(pre):
-                    return False
-            
-            visit.remove(crs)
-            preMap[crs] = []
-            return True
-        
-        # for crs in range(numCourses):
-        #     if not dfs(crs):
-        #         return False
-
-        # return True
-
-        # preMap = {i:[] for i in range(numCourses)}
-        # for crs, pre in prerequisites:
-        #     preMap[crs].append(pre)
-        
-        # visit = set()
-        # def dfs(crs):
-
-        #     if crs in visit:
-        #         return False
-
-        #     if preMap[crs] == []:
-        #         return True
-            
-        #     visit.add(crs)
-        #     for pre in preMap[crs]:
-        #         if not dfs(pre):
-        #             return False
-            
-        #     visit.remove(crs)
-        #     preMap[crs] = []
-        #     return True
-        
-        # for crs in range(numCourses):
-        #     if not dfs(crs):
-        #         return False
-        
-        # return True
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        # if len(prerequisites) == 0: return True
-        # indegree=[0] * numCourses
-        # starts = []
-        # graph = DefaultDict(list)
-        # for p in prerequisites:
-        #     indegree[p[0]] +=1
-        #     graph[p[1]]+=[p[0]]
-        # q = []
-        # for i in range(len(indegree)):
-        #     if indegree[i] == 0:
-        #         q.append(i)
-        # vis = 0
-      
-                
-        # while len(q)> 0:
-        #     top = q.pop(0)
-        #     vis+=1
-        #     for n in graph[top]:
-        #         indegree[n] -=1
-        #         if indegree[n] == 0:
-        #             q.append(n)
-        # return vis == numCourses
