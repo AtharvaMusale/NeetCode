1740623eab073b085ffb6110e1ca84c4ec2940d0
AtharvaMusale <46114095+AtharvaMusale@users.noreply.github.com>
2024-11-24 17:48:59 -0500

    Time: 112 ms (11.52%) | Memory: 27.4 MB (98.46%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.py
+++ 11-container-with-most-water/container-with-most-water.py
@@ -1,18 +1,15 @@
 class Solution:
     def maxArea(self, nums: List[int]) -> int:
-        l, r = 0, len(nums) - 1
-        max_area = 0
+        l, r = 0, len(nums) -1
+        res = 0
+        while l<r:
+            
+            curr_area =  (r-l) * min(nums[r],nums[l])
+            res = max(curr_area,res)
 
-        while l < r:
-            # Calculate the area for the current left and right pointers
-            height = min(nums[l], nums[r])
-            width = r - l
-            max_area = max(max_area, height * width)
-
-            # Move the pointer pointing to the shorter line inward
-            if nums[l] < nums[r]:
-                l += 1
+            if nums[l]<nums[r]:
+                l+=1
             else:
-                r -= 1
-                
-        return max_area
+                r-=1
+        
+        return res
