1a20b42a0ce43602e09d87bf547c7059818d8449
AtharvaMusale <46114095+AtharvaMusale@users.noreply.github.com>
2024-11-26 14:30:31 -0500

    Time: 631 ms (6.51%) | Memory: 17.3 MB (7.83%) - LeetSync

--- 16-3sum-closest/3sum-closest.py
+++ 16-3sum-closest/3sum-closest.py
@@ -1,17 +1,35 @@
+# class Solution:
+#     def threeSumClosest(self, nums: List[int], target: int) -> int:
+        
+#         nums.sort()
+#         closest = float('inf')  # Initialize with infinity
+#         for i in range(len(nums)-2):
+#             l, r = i+1, len(nums)-1
+#             while l<r:
+#                 add = nums[i] + nums[l] + nums[r]
+#                 if abs(add - target) < abs(closest- target):
+#                     closest  = add
+                
+#                 if add<target:
+#                     l+=1
+#                 else:
+#                     r-=1
+#         return closest
+
+
 class Solution:
     def threeSumClosest(self, nums: List[int], target: int) -> int:
-        
+        diff = float("inf")
         nums.sort()
-        closest = float('inf')  # Initialize with infinity
-        for i in range(len(nums)-2):
-            l, r = i+1, len(nums)-1
-            while l<r:
-                add = nums[i] + nums[l] + nums[r]
-                if abs(add - target) < abs(closest- target):
-                    closest  = add
-                
-                if add<target:
-                    l+=1
-                else:
-                    r-=1
-        return closest
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                complement = target - nums[i] - nums[j]
+                hi = bisect_right(nums, complement, j + 1)
+                lo = hi - 1
+                if hi < len(nums) and abs(complement - nums[hi]) < abs(diff):
+                    diff = complement - nums[hi]
+                if lo > j and abs(complement - nums[lo]) < abs(diff):
+                    diff = complement - nums[lo]
+            if diff == 0:
+                break
+        return target - diff
