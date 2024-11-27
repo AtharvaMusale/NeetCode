a478ad92d76b38fd277aa7c9fdf4cb4f9626c78a
AtharvaMusale <46114095+AtharvaMusale@users.noreply.github.com>
2024-11-27 12:09:16 -0500

    Time: 71 ms (71.42%) | Memory: 17.9 MB (5.01%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.py
+++ 76-minimum-window-substring/minimum-window-substring.py
@@ -1,31 +1,73 @@
+# from collections import defaultdict
+
+# class Solution:
+#     def minWindow(self, s: str, t: str) -> str:
+#         if t == "":
+#             return ""
+        
+#         countT, countS = {}, {}
+#         for c in t:
+#             countT[c] = 1 + countT.get(c , 0)
+
+#         have, need = 0 ,len(countT)
+#         l = 0
+#         res, resLen = [-1,-1], float('inf')
+
+#         for r in range(len(s)):
+#             c = s[r]
+#             countS[c] = 1+ countS.get(c,0)
+
+#             if c in countT and countS[c] == countT[c]:
+#                 have +=1
+            
+#             while have == need:
+#                 if (r-l+1) < resLen:
+#                     resLen = (r-l+1)
+#                     res = [l,r]
+#                 # Pop from left
+#                 countS[s[l]] -= 1
+#                 if s[l] in countT and countS[s[l]] < countT[s[l]]:
+#                     have -= 1
+#                 if countS[s[l]] == 0:
+#                     del countS[s[l]]
+#                 l+=1
+#         l,r = res
+#         return s[l:r+1] if resLen != float("inf") else ""
+
+
+from collections import defaultdict
+
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
         if t == "":
             return ""
-        
-        countT, countS = {}, {}
-        for c in t:
-            countT[c] = 1 + countT.get(c , 0)
 
-        have, need = 0 ,len(countT)
+        countT = defaultdict(int)
+        for c in t:
+            countT[c] += 1
+
+        countS = defaultdict(int)
+        have, need = 0, len(countT)
         l = 0
-        res, resLen = [-1,-1], float('inf')
+        res, resLen = [-1, -1], float('inf')
 
         for r in range(len(s)):
             c = s[r]
-            countS[c] = 1+ countS.get(c,0)
-
-            if c in countT and countS[c] == countT[c]:
-                have +=1
+            if c in countT:
+                countS[c] += 1
+                if countS[c] == countT[c]:
+                    have += 1
             
             while have == need:
-                if (r-l+1) < resLen:
-                    resLen = (r-l+1)
-                    res = [l,r]
-                # Pop from left
+                if (r - l + 1) < resLen:
+                    resLen = (r - l + 1)
+                    res = [l, r]
                 countS[s[l]] -= 1
                 if s[l] in countT and countS[s[l]] < countT[s[l]]:
                     have -= 1
-                l+=1
-        l,r = res
+                if countS[s[l]] == 0:
+                    del countS[s[l]]  # Delete zero count entries to save space
+                l += 1
+
+        l, r = res
         return s[l:r+1] if resLen != float("inf") else ""
