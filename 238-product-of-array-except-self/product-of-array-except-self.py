341d36bba6af80df170f07c745efd4dec3797f18
AtharvaMusale <46114095+AtharvaMusale@users.noreply.github.com>
2024-11-27 13:28:38 -0500

    Time: 15 ms (96.95%) | Memory: 22.6 MB (64.17%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.py
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -1,11 +1,29 @@
+# class Solution:
+#     def productExceptSelf(self, nums: List[int]) -> List[int]:
+#         # res = [1] * (len(nums))
+
+#         # for i in range(1, len(nums)):
+#         #     res[i] = res[i-1] * nums[i-1]
+#         # postfix = 1
+#         # for i in range(len(nums) - 1, -1, -1):
+#         #     res[i] *= postfix
+#         #     postfix *= nums[i]
+#         # return res
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        res = [1] * (len(nums))
+        n = len(nums)
+        output = [1] * n
 
-        for i in range(1, len(nums)):
-            res[i] = res[i-1] * nums[i-1]
-        postfix = 1
-        for i in range(len(nums) - 1, -1, -1):
-            res[i] *= postfix
-            postfix *= nums[i]
-        return res
+        # Left products
+        left_product = 1
+        for i in range(n):
+            output[i] = left_product
+            left_product *= nums[i]
+
+        # Right products
+        right_product = 1
+        for i in range(n-1, -1, -1):
+            output[i] *= right_product
+            right_product *= nums[i]
+
+        return output
