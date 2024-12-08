7ee2cea3660f3bbed014bdbefa96da59483ee2bb
AtharvaMusale <46114095+AtharvaMusale@users.noreply.github.com>
2024-12-08 12:53:24 -0500

    Time: 75 ms (97.97%) | Memory: 27.2 MB (57.02%) - LeetSync

--- 739-daily-temperatures/daily-temperatures.py
+++ 739-daily-temperatures/daily-temperatures.py
@@ -1,13 +1,29 @@
+# class Solution:
+#     def dailyTemperatures(self, t: List[int]) -> List[int]:
+#         stack = []
+#         res = [0] * len(t)
+
+#         for ind, val in enumerate(t):
+#             while stack and stack[-1][0] < val:
+#                 stackVal, stackInd = stack.pop()
+#                 res[stackInd] = ind - stackInd
+            
+#             stack.append((val, ind))
+
+#         return res
+
+
 class Solution:
     def dailyTemperatures(self, t: List[int]) -> List[int]:
-        stack = []
-        res = [0] * len(t)
+        n = len(t)
+        answer = [0] * n
+        stack = []  # This will store indices of t in decreasing order
 
-        for ind, val in enumerate(t):
-            while stack and stack[-1][0] < val:
-                stackVal, stackInd = stack.pop()
-                res[stackInd] = ind - stackInd
-            
-            stack.append((val, ind))
-
-        return res
+        for i, temp in enumerate(t):
+            # Check if the current temperature is warmer than the one at the top of the stack
+            while stack and t[stack[-1]] < temp:
+                prev_index = stack.pop()
+                answer[prev_index] = i - prev_index  # Calculate the number of days
+            stack.append(i)  # Push the current index onto the stack
+        
+        return answer
