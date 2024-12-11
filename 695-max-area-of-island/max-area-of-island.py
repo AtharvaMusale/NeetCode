06352d16481782c7b85a4d7e898c2783740b79b0
AtharvaMusale <46114095+AtharvaMusale@users.noreply.github.com>
2024-12-11 14:25:25 -0500

    Time: 27 ms (46.41%) | Memory: 19.3 MB (6.08%) - LeetSync

--- 695-max-area-of-island/max-area-of-island.py
+++ 695-max-area-of-island/max-area-of-island.py
@@ -1,81 +1,24 @@
 class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
-        rows = len(grid)
-        cols = len(grid[0])
-        vis =set()
+        ROWS, COLS = len(grid), len(grid[0])
+        visit = set()
 
-        def dfs(i,j):
-            if i<0 or i==rows or j<0 or  j==cols or grid[i][j] == 0 or (i,j) in vis:
+        def area(r, c):
+            if r < 0 or r >= ROWS or c < 0 or c >= COLS or (r, c) in visit or grid[r][c] == 0:
                 return 0
-            vis.add((i,j))
-            return 1+ dfs(i-1,j) + dfs(i+1,j) + dfs(i,j+1) + dfs(i,j-1)
 
-        area = 0
-        for i in range(rows):
-            for j in range(cols):
+            visit.add((r, c))
+            return (1 +
+                    area(r + 1, c) +
+                    area(r - 1, c) +
+                    area(r, c + 1) +
+                    area(r, c - 1))
 
-                area = max(area,dfs(i,j))
-        
-        return area
+        max_area = 0
+        for r in range(ROWS):
+            for c in range(COLS):
+                if grid[r][c] == 1 and (r, c) not in visit:
+                    current_area = area(r, c)
+                    max_area = max(max_area, current_area)
 
-
-
-
-# class Solution:
-#     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
-#         rows = len(grid)
-#         cols = len(grid[0])
-#         visit = set()
-
-#         def dfs(i,j):
-#             if i<0 or i==rows or j<0 or j==cols or grid[i][j]==0 or (i,j) in visit:
-#                 return 0
-            
-#             visit.add((i,j))
-#             return (1 + dfs(i+1,j) + dfs(i-1,j) + dfs(i,j+1) + dfs(i,j-1))
-
-        
-
-#         area = 0
-
-#         for i in range(rows):
-#             for j in range(cols):
-#                 area = max(area,dfs(i,j))
-
-#         return area
-
-
-
-
-
-
-
-
-
-
-
-        # m, n = len(grid), len(grid[0])
-        # dp = {}
-        # vis = set()
-        # def dfs(i, j):
-        #     if i<0 or j< 0 or i>=m or j>=n or grid[i][j] != 1 or (i, j) in vis:
-        #         return 0
-        #     # if (i, j) is dp: 
-        #     #     return dp[(i, j)]
-        #     vis.add((i, j))
-        #     maxArea = 1
-        #     for r, c in[(0,1),(1,0),(0,-1),(-1,0)]:
-        #         nr, nc = r+i, j+c
-        #         maxArea += dfs(nr, nc)
-        #     dp[(i, j)] = maxArea
-        #     return maxArea
-        # maxArea = 0
-        # for i in range(m):
-        #     for j in range(n):
-        #         if grid[i][j] == 1 and grid[i][j] not in vis:
-                   
-        #             maxArea = max(maxArea, dfs(i, j))
-        # return maxArea
-
-                
-        
+        return max_area
