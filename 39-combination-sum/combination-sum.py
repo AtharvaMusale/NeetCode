c17aaf9c7508f85568a142e4f412444e16734f8f
AtharvaMusale <46114095+AtharvaMusale@users.noreply.github.com>
2024-12-11 11:40:29 -0500

    Time: 11 ms (63.58%) | Memory: 17.5 MB (6.14%) - LeetSync

--- 39-combination-sum/combination-sum.py
+++ 39-combination-sum/combination-sum.py
@@ -1,46 +1,24 @@
 class Solution:
-    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        res = []
-        subset = [] 
+    def combinationSum(self, nums: List[int], target: int) -> List[List[int]]:
+        #  distinct, list unique comb, any order, same number can be chosen any no of times
 
-        def dfs(i,total):
-            if total ==target:
-                res.append(subset.copy())
+        res = []
+        subset = []
+
+        def backtrack(i, curr, total):
+            if total == target:
+                res.append(curr.copy())
                 return
             
-            if total>target or i>=len(candidates):
-                return 
-
-            # Include the current candidate and stay at the current index
-            subset.append(candidates[i])
-            dfs(i, total + candidates[i])
-            subset.pop()
-
-            # Exclude the current candidate and move to the next index
-            dfs(i + 1, total)
-
-
-        dfs(0,0)
-        return res
-
-# class Solution:
-#     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-       
-#         res = []
-#         subset = []
-
-#         def dfs(i, total):
-#             if total == target:
-#                 res.append(subset.copy())
-#                 return
+            if i>=len(nums) or total>target:
+                return
             
-#             if i>= len(candidates) or total>target:
-#                 return
-#             subset.append(candidates[i])
-#             dfs(i, total+ candidates[i])
-#             subset.pop()
+            curr.append(nums[i])
+            backtrack(i, curr, total+ nums[i])
+            curr.pop()
+            backtrack(i+1, curr, total)
 
-#             dfs(i+1, total)
-        
-#         dfs(0,0)
-#         return res
+        backtrack(0,[],0)
+
+        return res
+            
