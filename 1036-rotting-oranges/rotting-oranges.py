60602d793991e3d6b4db014d3cf5db87f8459482
AtharvaMusale <46114095+AtharvaMusale@users.noreply.github.com>
2024-12-11 12:07:35 -0500

    Time: 7 ms (28.74%) | Memory: 17.4 MB (9.69%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.py
+++ 1036-rotting-oranges/rotting-oranges.py
@@ -1,112 +1,34 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
+        # 0 empty, 1 orange 2 rotten
         ROWS, COLS = len(grid), len(grid[0])
-        time = 0
-        fresh = 0
-        q = deque()
+        # direction = 
+        q = collections.deque()
 
+        time = 0    
+        fresh = 0
+
+        
         for r in range(ROWS):
             for c in range(COLS):
                 if grid[r][c] == 1:
                     fresh+=1
-                if grid[r][c]==2:
-                    q.append([r,c])
-            
-        directions = [[1,0],[0,1],[-1,0],[0,-1]]
+                if grid[r][c] == 2:
+                    q.append((r,c))
+        
+        directions = [[-1,0],[1,0],[0,1],[0,-1]]
         while q and fresh>0:
-            length = len(q)
-            for i in range(len(q)):
+            n = len(q)
+            for i in range(n):
                 r,c = q.popleft()
                 for dr,dc in directions:
-                    row = r + dr
-                    col = c + dc
-                    if (row in range(ROWS)) and (col in range(COLS)) and grid[row][col] == 1:
+                    row,col = r+dr, c+dc
+                    if row in range(ROWS) and col in range(COLS) and grid[row][col] == 1:
+
+                        fresh-=1
+                        q.append((row,col))
                         grid[row][col] = 2
-                        fresh -= 1
-                        q.append([row,col])
+                    
             time+=1
-        return time if fresh == 0 else -1
-
-
-
-
-
-
-        # q = deque()
-        # fresh = 0
-        # time = 0
-
-        # for i in range(len(grid)):
-        #     for j in range(len(grid[0])):
-        #         if grid[i][j] == 1:
-        #             fresh+=1
-                
-        #         if grid[i][j] == 2:
-        #             q.append((i,j))
-                
-        # directions = [[1,0],[0,1],[-1,0],[0,-1]]
-
-        # while q and fresh>0:
-        #     length= len(q)
-        #     for i in range(length):
-        #         r,c = q.popleft()
-        #         for dr, dc in directions:
-        #             row = r+dr
-        #             col = c+dc
-        #             if (row in range(len(grid)) and col in range(len(grid[0])) and grid[row][col] == 1):
-        #                 grid[row][col] = 2
-        #                 fresh -= 1
-        #                 q.append((row,col))
-        #     time+=1
-        
-        # return time if fresh == 0 else -1
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        # q = []
-        # oranges =0
-        # for i in range(len(grid)):
-        #     for j in range(len(grid[0])):
-        #         if grid[i][j] != 0:
-        #             oranges+=1
-        #             if grid[i][j] == 2:
-        #                 q.append((i, j, 0))
-        #                 oranges -=1
-        # vis=[]    
-        # maxtime = 0   
-        # while(q):
-        #     row, col, time = q.pop(0)
-            
-        #     print(row, col, time)
-        #     for i, j in ((0,1),(1,0),(0,-1),(-1,0)):
-        #         nr, nc = row+i, col+j
-        #         if 0<=nr<len(grid) and 0<=nc<len(grid[0]) and grid[nr][nc] == 1:
-        #             grid[nr][nc] = 2
-        #             q.append((nr,nc,time+1))
-        #             maxtime=max(maxtime, time+1)
-        #             oranges -= 1
-        # return maxtime if oranges == 0 else -1
-
-
+        return time if fresh==0 else -1
 
