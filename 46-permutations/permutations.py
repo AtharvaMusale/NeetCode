4e380337c928b78cc5a810de7d7216875798d4e1
AtharvaMusale <46114095+AtharvaMusale@users.noreply.github.com>
2024-12-11 21:10:41 -0500

    Time: 0 ms (100.00%) | Memory: 17.3 MB (17.90%) - LeetSync

--- 46-permutations/permutations.py
+++ 46-permutations/permutations.py
@@ -1,78 +1,15 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
+        # [1,2,3] = []
+
+        if len(nums) == 0:
+            return [[]]
+        
+        perms = self.permute(nums[1:])
         res = []
-        if len(nums)== 1:
-            return [nums[:]]    
-        for i in range(len(nums)):
-            n = nums.pop(0)
-            perms = self.permute(nums)
-            for perm in perms:
-                perm.append(n)
-            res.extend(perms)
-            nums.append(n)
+        for p in perms:
+            for i in range(len(p)+1):
+                p_copy = p.copy()
+                p_copy.insert(i,nums[0])
+                res.append(p_copy)
         return res
-
-
-
-# class Solution:
-#     def permute(self, nums: List[int]) -> List[List[int]]:
-#         res = []
-#         if len(nums)== 1:
-#             return [nums[:]]     
-#         for i in range(len(nums)):
-#             n = nums.pop(0)
-#             perms = self.permute(nums)
-#             for perm in perms:
-#                 perm.append(n)
-#             res.extend(perms)
-#             nums.append(n)
-#         return res
-
-# class Solution:
-#     def permute(self, nums: List[int]) -> List[List[int]]:
-#         res = []
-#         if len(nums)== 1:
-#             return [nums[:]]    
-#         for i in range(len(nums)):
-#             n = nums.pop(0)
-#             perms = self.permute(nums)
-#             for perm in perms:
-#                 perm.append(n)
-#             res.extend(perms)
-#             nums.append(n)
-#         return res
-
-
-
-
-
-
-
-
-
-
-        # class Solution:
-#     def __init__(self):
-#         self.result = []
-#     def permute(self, nums: List[int]) -> List[List[int]]:
-#         vis = set()
-#         curr = []
-
-#         self.solve(curr, vis, nums)
-
-#         return self.result
-
-#     def solve(self, curr, vis, nums):
-#         if len(curr) == len(nums):
-#             self.result.append(curr.copy())
-#             return
-        
-#         for i in range(len(nums)):
-#             if i not in vis:
-#                 curr.append(nums[i])
-#                 vis.add(i)
-#                 self.solve(curr, vis, nums)
-#                 curr.pop()
-#                 vis.remove(i)
-        
-
