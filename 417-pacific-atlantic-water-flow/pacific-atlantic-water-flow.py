fa7a3128898e1bc37f8ec2c7854bf84f93a9fd5a
AtharvaMusale <46114095+AtharvaMusale@users.noreply.github.com>
2024-12-12 15:51:52 -0500

    Time: 35 ms (52.48%) | Memory: 19 MB (9.33%) - LeetSync

--- 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.py
+++ 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.py
@@ -27,3 +27,32 @@ class Solution:
                 if (r,c) in pac and (r,c) in atl:
                     res.append([r,c])
         return res
+
+        pac, atl = set(), set()
+        ROWS, COLS = len(heights), len(heights[0])
+        res = []
+
+        # def dfs(r,c,visit,prevH):
+        #     if r<0 or r>=ROWS or c<0 or c>=COLS or (r,c) in visit or heights[r][c] < prevH:
+        #         return 
+        #     visit.add((r,c))
+        #     dfs(r+1,c,visit, prevH)
+        #     dfs(r-1,c,visit, prevH)
+        #     dfs(r,c+1,visit, prevH)
+        #     dfs(r,c-1,visit, prevH)
+
+
+        # for c in range(COLS):
+        #     dfs(0,c,pac,heights[0][c])
+        #     dfs(ROWS-1, c, atl, heights[ROWS-1][c])
+        
+        # for r in range(ROWS):
+        #     dfs(r,0,pac,heights[r][0])
+        #     dfs(r, COLS-1, atl, heights[r][COLS-1])
+        
+        # for r in range(ROWS):
+        #     for c in range(COLS):
+        #         if (r,c) in pac and (r,c) in atl:
+        #             res.append([r,c])
+        
+        # return res
