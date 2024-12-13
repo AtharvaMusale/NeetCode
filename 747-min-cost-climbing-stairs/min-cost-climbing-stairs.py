0f49d38207b97829a9a96b70fee7bfe4bb685952
AtharvaMusale <46114095+AtharvaMusale@users.noreply.github.com>
2024-12-13 11:13:58 -0500

    Time: 0 ms (100.00%) | Memory: 17.3 MB (29.32%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.py
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.py
@@ -1,43 +1,6 @@
 class Solution:
-    def minCostClimbingStairs(self, arr: List[int]) -> int:
-        # for i in range(len(nums)-3,-1,-1):
-        #     nums[i] += min(nums[i+1] , nums[i+2])
-
-        # return min(nums[0],nums[1])     
-        memo = {}
-        def recur(i):
-            if i>=len(arr):
-                return 0
-            if i in memo:
-                return memo[i]
-            
-            cost = arr[i] + min(recur(i+1) , recur(i+2))
-
-            memo[i] = cost
-            return cost
-        
-        return min(recur(0),recur(1))
-        
-            
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        # for i in range(len(cost)-3,-1,-1):
-        #     cost[i] += min(cost[i+1],cost[i+2])
-        
-        # return min(cost[0],cost[1])
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        cost.append(0)
+        for i in range(len(cost)-3,-1,-1):
+            cost[i] += min(cost[i+1],cost[i+2])
+        return min(cost[0],cost[1])
