dec688fca7467e0169267ad1c65f36899e556e51
AtharvaMusale <46114095+AtharvaMusale@users.noreply.github.com>
2024-12-14 11:41:02 -0500

    Time: 118 ms (33.55%) | Memory: 17.6 MB (17.84%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
@@ -1,49 +1,20 @@
+from collections import defaultdict
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        res = 0
+        char_count = defaultdict(int)
+        window_size = 0
+        max_len = 0
+        max_freq = 0
         l = 0
-        hashset ={}
 
         for r in range(len(s)):
-            hashset[s[r]] = 1 + hashset.get(s[r],0)
-
-            while (r-l+1) - max(hashset.values()) > k:
-                hashset[s[l]] -= 1
+            char_count[s[r]]+=1
+            max_freq = max(max_freq, char_count[s[r]])
+            window_size = r-l+1
+            if (window_size-max_freq)>k:
+                char_count[s[l]]-=1
                 l+=1
-            res = max(res,(r-l+1))
-        return res
-        # res = 0
-        # l = 0
-        # hashset = {}
-        # for r in range(len(s)):
-        #     hashset[s[r]] = 1 + hashset.get(s[r],0)
-
-        #     while (r-l+1) - max(hashset.values()) > k:
-        #         hashset[s[l]] -= 1
-        #         l+=1
-
-        #     res = max(res, r-l+1)
-        # return res
             
-        # freq = defaultdict(int)
-        # maxFreq = 0
-        # maxLen = 0
-        # i, j = 0, 0
-        # while i < len(s) and j < len(s):
-        #     freq[s[j]] += 1
-        #     maxFreq = max(maxFreq, freq[s[j]])
-        #     len_s = j-i+1
-        #     isValid = len_s - maxFreq <= k
-        #     if not isValid:
-        #         freq[s[i]] -= 1
-        #         i+=1
-        #     maxLen = max(j-i+1, maxLen)
-        #     j+=1
-            
-        # return maxLen
-
-                
-
-
-            
+            max_len = max(max_len,r-l+1)
         
+        return max_len
