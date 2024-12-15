eb9b7dfd6fc68e42bea44329569007ee7806a887
AtharvaMusale <46114095+AtharvaMusale@users.noreply.github.com>
2024-12-15 13:54:25 -0500

    Time: 118 ms (44.62%) | Memory: 51 MB (40.96%) - LeetSync

--- 435-non-overlapping-intervals/non-overlapping-intervals.py
+++ 435-non-overlapping-intervals/non-overlapping-intervals.py
@@ -1,32 +1,27 @@
 class Solution:
-    def eraseOverlapIntervals(self, intervals: List[List[int]]) -> int:
-        intervals.sort()
-        base = intervals[0][1]
-        res = 0
-        for i in range(1,len(intervals)):
-            curr = intervals[i]
-
-            if curr[0]<base:
-                res+=1
-                base = min(base,curr[1])
-            
-            else:
-                base = curr[1]
-
-        return res
-
-        # intervals.sort()
-
-        # currMinEnd = intervals[0][1]
+    def eraseOverlapIntervals(self, arr: List[List[int]]) -> int:
+        # intervals.sort(key = lambda x:x[0])
+        # prevEnd = intervals[0][1]
         # res = 0
 
-        # for i in range(1,len(intervals)):
-        #     currInterval = intervals[i]
-
-        #     if currInterval[0] < currMinEnd:        
-        #         res += 1
-        #         currMinEnd = min(currMinEnd, currInterval[1])
+        # for start, end in intervals[1:]:
+        #     if start >= prevEnd:
+        #         prevEnd = end
+            
         #     else:
-        #         currMinEnd = currInterval[1]
-
+        #         res+=1
+        #         prevEnd = min(end,prevEnd)
+            
         # return res
+
+        arr.sort(key = lambda x:x[0])
+        prevEnd = arr[0][1]
+        res = 0
+        for start, end in arr[1:]:
+            if start>=prevEnd:
+                prevEnd = end
+            else:
+                res+=1
+                prevEnd = min(prevEnd,end)
+            
+        return res
