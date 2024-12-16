81969241dc21d80b42eb90088df447cd6b012fd1
AtharvaMusale <46114095+AtharvaMusale@users.noreply.github.com>
2024-12-16 13:05:13 -0500

    Time: 130 ms (52.44%) | Memory: 19.3 MB (7.10%) - LeetSync

--- 621-task-scheduler/task-scheduler.py
+++ 621-task-scheduler/task-scheduler.py
@@ -1,51 +1,22 @@
 class Solution:
     def leastInterval(self, tasks: List[str], n: int) -> int:
         count = Counter(tasks)
-        maxHeap = [-cnt for cnt in count.values()]
-        heapq.heapify(maxHeap)
+        maxheap = [-cnt for cnt in count.values()]
+        heapq.heapify(maxheap)
+    
         time = 0
-        q = collections.deque()
+        q = deque()
 
-        while maxHeap or q:
+        while maxheap or q:
             time+=1
 
-            if maxHeap:
-                cnt = 1 + heapq.heappop(maxHeap)
+            if maxheap:
+                cnt = 1 + heapq.heappop(maxheap)
 
                 if cnt:
-                    q.append([cnt,time+n])
-            
+                    q.append([cnt, time + n])
+                
             if q and q[0][1] == time:
-                heapq.heappush(maxHeap,q.popleft()[0])
+                heapq.heappush(maxheap, q.popleft()[0])
             
         return time
-
-
-
-
-
-
-# from heapq import heappush, heappop, heapify
-# class Solution:
-#     def leastInterval(self, tasks: List[str], n: int) -> int:
-#         cnt = Counter(tasks)
-#         maxHeap = [-c for c in cnt.values()]
-#         heapq.heapify(maxHeap)
-#         time = 0
-#         q = deque()
-#         while maxHeap or q:
-#             time+=1
-
-#             if maxHeap:
-#                 cnt = 1 + heapq.heappop(maxHeap)
-            
-#                 if cnt:
-#                     q.append([cnt,time+n])
-            
-#             if q and q[0][1] == time:
-#                 heapq.heappush(maxHeap,q.popleft()[0])
-                
-#         return  time
-
-      
-        
