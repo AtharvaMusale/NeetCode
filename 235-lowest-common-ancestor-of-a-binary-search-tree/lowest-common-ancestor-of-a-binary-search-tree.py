f569f1d85aa09a0e2106bd88fce4960b093c2a0f
AtharvaMusale <46114095+AtharvaMusale@users.noreply.github.com>
2024-12-29 12:48:31 -0500

    Time: 67 ms (6.28%) | Memory: 21.3 MB (9.59%) - LeetSync

--- 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.py
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.py
@@ -1,30 +1,20 @@
-# # Definition for a binary tree node.
-# # class TreeNode:
-# #     def __init__(self, x):
-# #         self.val = x
-# #         self.left = None
-# #         self.right = None
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
 
-# class Solution:
-#     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-#         if p.val>root.val and q.val>root.val:
-#             return self.lowestCommonAncestor(root.right, p, q)
-        
-#         elif p.val < root.val and q.val< root.val:
-#             return self.lowestCommonAncestor(root.left,p,q)
-        
-#         else:
-#             return root
-        
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        while root:
-            # Move to the right subtree if both p and q are greater than root.
-            if p.val > root.val and q.val > root.val:
-                root = root.right
-            # Move to the left subtree if both p and q are lesser than root.
-            elif p.val < root.val and q.val < root.val:
-                root = root.left
-            else:
-                # Found the split point or one of p or q is the root.
-                return root
+        if not root:
+            return None
+        
+        if p.val<root.val and q.val< root.val:
+            return self.lowestCommonAncestor(root.left, p, q)
+        
+        elif p.val > root.val and q.val > root.val:
+            return self.lowestCommonAncestor(root.right, p, q)
+
+        else:
+            return root
