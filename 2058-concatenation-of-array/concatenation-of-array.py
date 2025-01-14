cea22dea3e439680f5698e31d27badaa03a63a17
AtharvaMusale <46114095+AtharvaMusale@users.noreply.github.com>
2025-01-14 12:17:42 -0500

    Time: 0 ms (100.00%) | Memory: 17.8 MB (43.11%) - LeetSync

--- 2058-concatenation-of-array/concatenation-of-array.py
+++ 2058-concatenation-of-array/concatenation-of-array.py
@@ -1,5 +1,5 @@
-class Solution:
-    def getConcatenation(self, nums: List[int]) -> List[int]:
+# class Solution:
+#     def getConcatenation(self, nums: List[int]) -> List[int]:
         # For empty array?
         # Could there be duplicates?
         # Could there be egative numbers
@@ -7,9 +7,16 @@ class Solution:
         
         # Approach: Create an array of 2n len with all 0s
         # Use Two Pointers and edit
-        ans = [0] * (2 * len(nums))
-        for i in range(len(nums)):
-            ans[i] = nums[i]
-            ans[i+len(nums)] = nums[i]
+        # ans = [0] * (2 * len(nums))
+        # for i in range(len(nums)):
+        #     ans[i] = nums[i]
+        #     ans[i+len(nums)] = nums[i]
         
+        # return ans
+class Solution:
+    def getConcatenation(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        ans = [0] * (2 * n)
+        for i, num in enumerate(nums):
+            ans[i] = ans[i + n] = num
         return ans
