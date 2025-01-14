ea21907286a0a8f4d820873f9c0861a4a56692cd
AtharvaMusale <46114095+AtharvaMusale@users.noreply.github.com>
2025-01-14 14:45:12 -0500

    Time: 10 ms (86.85%) | Memory: 20.6 MB (60.29%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,37 +1,10 @@
-# class Solution:
-#     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        
-#         res = defaultdict(list)
-#         for s in strs:
-#             count = [0] * 26
-#             for c in s:
-#                 count[ord(c)- ord("a")] += 1
-            
-#             res[tuple(count)].append(s)
-#         return res.values()
-
-# class Solution:
-#     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:    
-#         res = collections.defaultdict(list)
-#         for i in strs:
-#             count = [0] * 26
-#             for c in i:
-#                 count[ord(c) - ord("a")] +=1
-#             res[tuple(count)].append(i)
-#         return list(res.values())
-   
-
+from collections import defaultdict
 class Solution:
+
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-            
-        from collections import defaultdict
-        anagrams = defaultdict(list)
-        
-        for s in strs:
-            # Sort each string to form the key
-            key = ''.join(sorted(s))
-            anagrams[key].append(s)
-        
-        # Return grouped anagrams as a list of lists
-        return list(anagrams.values())
-    
+        # Returned in the form of list of list
+        hashmap = defaultdict(list)
+        for i in strs:
+            key = "".join(sorted(i))
+            hashmap[key].append(i)
+        return list(hashmap.values())
