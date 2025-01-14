7ab38f581f1e2e99463cbd73c24f2512c69bf040
AtharvaMusale <46114095+AtharvaMusale@users.noreply.github.com>
2025-01-14 12:16:27 -0500

    Time: 1 ms (23.33%) | Memory: 17.9 MB (43.11%) - LeetSync

--- 2058-concatenation-of-array/concatenation-of-array.py
+++ 2058-concatenation-of-array/concatenation-of-array.py
@@ -1,5 +1,15 @@
 class Solution:
     def getConcatenation(self, nums: List[int]) -> List[int]:
-
-        nums.extend(nums)  # Extend the list with itself
-        return nums
+        # For empty array?
+        # Could there be duplicates?
+        # Could there be egative numbers
+        # Can I use extra space?
+        
+        # Approach: Create an array of 2n len with all 0s
+        # Use Two Pointers and edit
+        ans = [0] * (2 * len(nums))
+        for i in range(len(nums)):
+            ans[i] = nums[i]
+            ans[i+len(nums)] = nums[i]
+        
+        return ans
