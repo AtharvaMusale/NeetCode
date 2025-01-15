8099fa20108690d738dde9b057190290bc412392
AtharvaMusale <46114095+AtharvaMusale@users.noreply.github.com>
2025-01-15 13:15:57 -0500

    Time: 435 ms (92.03%) | Memory: 20.7 MB (42.35%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -1,55 +1,37 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
+        # Can there be negative numbers?
+        # Can there be duplicates?
+        # 
+
+        # Approach:
+        # i,j,k 3 ptr, Sort the input -> Fix One Pointer -> Use other two for Condition check
+
         res = []
         nums.sort()
 
+        # [-4,-4,-1,0,1,2]
         for i in range(len(nums)-2):
+            #-4
+            if nums[i]>0:
+                break
+            
             if i>0 and nums[i] == nums[i-1]:
                 continue
-            
+
             l, r = i+1, len(nums)-1
+
             while l<r:
                 add = nums[i] + nums[l] + nums[r]
-                if add < 0:
-                    l+=1
-                elif add > 0:
+                if add>0:
                     r-=1
-                
+                elif add<0:
+                    l+=1
                 else:
                     res.append([nums[i],nums[l],nums[r]])
-                    while l<r and nums[l] == nums[l+1]:
-                        l+=1
-                    
-                    while l<r and nums[r] == nums[r-1]:
-                        r-=1
-                    
                     l+=1
                     r-=1
+                    while nums[l] == nums[l-1] and l<r:
+                        l+=1
         return res 
-
-
-
-        # res = []
-        # nums.sort()
-
-        # for i in range(len(nums)-2):
-        #     if i >0 and nums[i] == nums[i-1]:
-        #         continue
-            
-        #     l, r = i+1, len(nums)-1
-        #     while l<r:
-        #         total = nums[i] + nums[l] + nums[r]
-        #         if total < 0:
-        #             l+=1
-        #         elif total > 0:
-        #             r-=1
-        #         else:
-        #             res.append([nums[i], nums[l], nums[r]])
-        #             while l<r and nums[l] == nums[l+1]:
-        #                 l+= 1
-        #             while l<r and nums[r]==nums[r-1]:
-        #                 r-=1
-        #             l+=1
-        #             r-=1
-        # return res
 
