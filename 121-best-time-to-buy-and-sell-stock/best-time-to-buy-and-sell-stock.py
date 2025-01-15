3c11481d4b195796560cdeeba2b7242cd4e1aff2
AtharvaMusale <46114095+AtharvaMusale@users.noreply.github.com>
2025-01-15 13:39:43 -0500

    Time: 97 ms (49.97%) | Memory: 26.8 MB (39.40%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -1,61 +1,18 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        if not prices:
-            return 0
+        # Return profit
+        # Can there be negative prices? No
+        #  Duplicates? Maybe
         
-        curr_profit = 0
-        max_profit = 0
-        min_value = float("inf")
-
-        for i in range(len(prices)):
-            if prices[i] < min_value:
-                min_value = prices[i]
-            
-            if prices[i] - min_value > max_profit:
-                curr_profit = prices[i] - min_value
-            
-            max_profit = max(curr_profit, max_profit)
-        
-        return max_profit
-
-
-        # if not prices:
-        #     return 0
-        
-        # curr_profit = 0
-        # max_profit = 0
-        # min_value = float("inf")
-
-        # for i in range(len(prices)):
-        #     if prices[i]<min_value:
-        #         min_value = prices[i]
-        #     if prices[i]-min_value > max_profit:
-        #         curr_profit = prices[i]-min_value
-            
-        #     max_profit = max(max_profit, curr_profit)
-
-        # return max_profit
-        
-
-
-
-# class Solution:
-#     def maxProfit(self, prices: List[int]) -> int:
-
-#         if not prices:
-#             return 0
-        
-#         min_val = float("inf")
-#         profit = 0
-#         curr_profit = 0
-#         for i in range(len(prices)):
-#             if prices[i] < min_val:
-#                 min_val = prices[i]
-            
-#             if prices[i] - min_val > profit:
-#                 curr_profit = prices[i] - min_val
-            
-#             profit = max(profit,curr_profit)
-#         return profit
-
-
+        # Approach:
+        # [7,1,5,3,6,4] : l=3,r=6 profit = 7
+        maxP = 0
+        l,r = 0,1
+        while r<len(prices):
+            if prices[l]<prices[r]:
+                profit = prices[r] - prices[l]
+                maxP = max(profit,maxP)
+            else:
+                l = r
+            r +=1
+        return maxP
