3b9fcdc7a96bd04bc443dd207efdfc28916444c9
AtharvaMusale <46114095+AtharvaMusale@users.noreply.github.com>
2025-01-16 08:08:45 -0500

    Time: 0 ms (100.00%) | Memory: 18.2 MB (14.10%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.py
+++ 22-generate-parentheses/generate-parentheses.py
@@ -7,21 +7,21 @@ class Solution:
         stack = []
         res = []
 
-        def dfs(openN,closedN):
-            if openN == closedN == n:
+        def backtrack(open_count, close_count):
+            
+            if open_count == close_count == n:
                 res.append("".join(stack))
                 return
-            
 
-            if openN < n:
+            if open_count < n:
                 stack.append("(")
-                dfs(openN+1,closedN)
+                backtrack(open_count + 1, close_count)
                 stack.pop()
-            
-            if closedN<openN:
+
+            if close_count < open_count:
                 stack.append(")")
-                dfs(openN, closedN+1)
+                backtrack(open_count, close_count + 1)
                 stack.pop()
         
-        dfs(0,0)
+        backtrack(0, 0)
         return res
