986ed8edc62c92891b9243ad12799aaa26be4c6a
AtharvaMusale <46114095+AtharvaMusale@users.noreply.github.com>
2025-01-17 10:19:48 -0500

    Time: 0 ms (100.00%) | Memory: 18 MB (35.51%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
@@ -1,24 +1,44 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        l , r  = 0, len(nums)-1
+        # l, r = 0, len(nums)-1
+
+        # while l<=r:
+        #     mid = (l+r)//2
+
+        #     if nums[mid] ==target:
+        #         return mid
+            
+        #     if nums[l]<=nums[mid]:
+        #         if nums[l]<=target<nums[mid]:
+        #             r = mid -1
+        #         else:
+        #             l = mid+1
+                
+        #     else:
+        #         if nums[mid]< target<=nums[r]:
+        #             l = mid+1
+        #         else:
+        #             r  = mid - 1
+
+        # return -1
+
+
+        l, r = 0, len(nums)-1
 
         while l<=r:
-            mid = (l+r)//2
-
-            if target == nums[mid]:
-                return mid
-            
-            # Are we in left side of the array?:
-            if nums[l] <= nums[mid]:
-                if target > nums[mid] or target < nums[l]:
-                    l = mid+1
+            m = (l+r)//2
+            if nums[m] == target:
+                return m
+            if nums[l] <= nums[m]:
+                if nums[l]<=target<nums[m]:
+                    r = m -1
                 else:
-                    r = mid - 1
-            
+                    l = m + 1
             else:
-                if target < nums[mid] or target > nums[r]:
-                    r = mid -1
+                if nums[m] < target <= nums[r]:
+                    l = m + 1
                 else:
-                    l = mid + 1
-
+                    r = m - 1
+                    
         return -1
+
