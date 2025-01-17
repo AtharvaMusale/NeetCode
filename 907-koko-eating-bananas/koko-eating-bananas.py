a79cafab13001d846d3d7a5effcf66b03ec19a70
AtharvaMusale <46114095+AtharvaMusale@users.noreply.github.com>
2025-01-17 09:13:17 -0500

    Time: 218 ms (12.04%) | Memory: 18.9 MB (26.43%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.py
+++ 907-koko-eating-bananas/koko-eating-bananas.py
@@ -1,22 +1,40 @@
+# class Solution:
+#     def minEatingSpeed(self, piles: List[int], h: int) -> int:
+#         # [3,6,7,11] [1, 2, 2, 3]
+#         # HOURS = INT(PILE[I]/K) <= h if greater then increase k
+        
+#         l, r = 1 ,  max(piles)
+#         res = r
+
+#         while l<=r:
+#             k = l + (r-l)//2
+            
+#             total = 0
+#             for p in piles:
+#                 total += math.ceil(int(p)/k)
+
+#             if total > h:
+#                 l = k + 1
+#             else:
+#                 res = k
+#                 r = k - 1
+
+#         return res            
+
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
-        # [3,6,7,11] [1, 2, 2, 3]
-        # HOURS = INT(PILE[I]/K) <= h if greater then increase k
-        
-        l, r = 1 ,  max(piles)
+        l, r = 1, max(piles)
         res = r
 
-        while l<=r:
-            k = l + (r-l)//2
-            
-            total = 0
-            for p in piles:
-                total += math.ceil(int(p)/k)
+        while l <= r:
+            k = (l + r) // 2
 
-            if total > h:
-                l = k + 1
-            else:
+            totalTime = 0
+            for p in piles:
+                totalTime += math.ceil(float(p) / k)
+            if totalTime <= h:
                 res = k
                 r = k - 1
-
-        return res            
+            else:
+                l = k + 1
+        return res
