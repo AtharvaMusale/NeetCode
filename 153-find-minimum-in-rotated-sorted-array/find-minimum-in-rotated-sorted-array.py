a2ef8de1171a34c1b76029aacc365aac9c4ac06c
AtharvaMusale <46114095+AtharvaMusale@users.noreply.github.com>
2025-01-17 09:24:50 -0500

    Time: 0 ms (100.00%) | Memory: 18.2 MB (17.19%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.py
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.py
@@ -1,10 +1,18 @@
 class Solution:
     def findMin(self, nums: List[int]) -> int:
-        l, r = 0, len(nums) - 1
-        while l < r:
-            m = l + (r - l) // 2
-            if nums[m] < nums[r]:
-                r = m
-            else:
+        # [4,5,6,7,0,1,2] Rotated 4 times
+        # [0,1,2,4,5,6,7] Rotated 0 or 7 times
+        # Unique Elements, Return Min Element
+        # If mid element > rth element: Right array is sorted and smaller so we go right
+        #  Else Left
+        #  Can there be duplicates?
+
+        l , r = 0, len(nums)-1
+        while l<r:
+            m = (l+r)//2
+            if nums[m]>=nums[r]:
                 l = m + 1
+            else:
+                r = m
         return nums[l]
+
