ee4e9a73ef798c2b6d15dfd3875ab5fbdc9983cb
AtharvaMusale <46114095+AtharvaMusale@users.noreply.github.com>
2025-01-18 12:55:27 -0500

    Time: 91 ms (49.02%) | Memory: 28.5 MB (48.99%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.py
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.py
@@ -1,13 +1,10 @@
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
-        # nums = [1,6,2,5,8,1], k = 3
-        # out= [8,6,5]
-        # nums = [-1,-6,-2,-5,-8,-1]
-        # [-8,-6,-5,-2,-1,-1]
         nums = [-i for i in nums]
+        # [-3,-2,-1,-5,-6,-4]
         heapq.heapify(nums)
-
-        for i in range(k - 1):  # Pop k-1 times to get to the k-th largest element
+        # [-6,-5,-4,-3,-2,-1]
+        for i in range(k-1):
             heapq.heappop(nums)
         
-        return -heapq.heappop(nums)  # Return the negative of the k-th pop to revert the initial negation
+        return -nums[0]
