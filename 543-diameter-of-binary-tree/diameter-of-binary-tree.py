55a51db5a4d0f421b34ba55a417b036850efce06
AtharvaMusale <46114095+AtharvaMusale@users.noreply.github.com>
2025-01-21 07:56:13 -0500

    Time: 4 ms (73.10%) | Memory: 20.8 MB (31.75%) - LeetSync

--- 543-diameter-of-binary-tree/diameter-of-binary-tree.py
+++ 543-diameter-of-binary-tree/diameter-of-binary-tree.py
@@ -1,43 +1,25 @@
-# # Definition for a binary tree node.
-# # class TreeNode:
-# #     def __init__(self, val=0, left=None, right=None):
-# #         self.val = val
-# #         self.left = left
-# #         self.right = right
-# class Solution:
-#     def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
-#         self.res = 0
-
-#         def dfs(curr):
-#             if not curr:
-#                 return 0
-
-#             lh = dfs(curr.left)
-#             rh = dfs(curr.right)
-
-#             self.res = max(self.res, lh+rh)
-        
-#             return 1 + max(lh,rh)
-    
-#         dfs(root)
-#         return self.res
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
     def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
         self.res = 0
-        
-        def dfs(curr):
-            if not curr:
+
+        def dfs(root):
+            if not root:
                 return 0
+            
+            lh = dfs(root.left)
+            rh = dfs(root.right)
 
-            # Recursively get the depth of the left and right subtrees
-            lh = dfs(curr.left)
-            rh = dfs(curr.right)
+            self.res = max(self.res,lh+rh)
 
-            # Update the result with the maximum diameter found
-            self.res = max(self.res, lh + rh)
-
-            # Return the depth of the current node
-            return 1 + max(lh, rh)
+            return 1 + max(lh,rh)
         
         dfs(root)
+
         return self.res
+
