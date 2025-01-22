5784e0e92c050c26910886e2f931b75ebb077df8
AtharvaMusale <46114095+AtharvaMusale@users.noreply.github.com>
2025-01-22 11:33:23 -0500

    Time: 27 ms (83.53%) | Memory: 20.4 MB (47.20%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.py
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.py
@@ -1,30 +1,22 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
-        res = 0
-        currSum = 0
-        prefixSum = {0:1}
-        for i in range(len(nums)):
-            currSum += nums[i]
-            diff =  currSum - k
+        hashmap = {0:1}
+        curr = 0
+        count = 0
 
-            res += prefixSum.get(diff,0)
-            prefixSum[currSum] = 1+ prefixSum.get(currSum,0)
-        return res
+        # Check the map
+        for i in nums:
+            curr+=i
+            req_sum = curr - k
 
-
-
-        # hashmap = defaultdict(int) #cumulative sum up to all the indices possible
-        # along with the number of times the same sum occurs. sum[i] -sum[j] = k, then sum[i]-k = sum[j]
-
-        # psum = 0
-        # ans = 0
-        # hashmap[0] = 1
-
-        # for n in nums:
-        #     psum += n
-        #     if psum-k in hashmap:
-        #         ans += hashmap[psum-k]
+            if req_sum in hashmap:
+                count+=hashmap[req_sum]
             
-        #     hashmap[psum] = hashmap[psum]+1
-        
-        # return ans
+            if curr in hashmap:
+                hashmap[curr] += 1
+            
+            else:
+                hashmap[curr] = 1
+            
+        return count
+
