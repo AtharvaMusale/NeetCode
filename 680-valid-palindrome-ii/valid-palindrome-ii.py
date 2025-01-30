da703c434636462167e6005a021f92bc95374352
AtharvaMusale <46114095+AtharvaMusale@users.noreply.github.com>
2025-01-30 12:33:59 -0500

    Time: 62 ms (57.70%) | Memory: 17.9 MB (68.20%) - LeetSync

--- 680-valid-palindrome-ii/valid-palindrome-ii.py
+++ 680-valid-palindrome-ii/valid-palindrome-ii.py
@@ -1,20 +1,17 @@
 class Solution:
     def is_palindrome(self, s, l, r):
-        while l < r:  # Change l <= r to l < r
-            if s[l] != s[r]:
-                return False
-            l += 1
-            r -= 1
-        return True
-        
-    def validPalindrome(self, s: str) -> bool:
-        l, r = 0, len(s) - 1
-        
         while l < r:
             if s[l] != s[r]:
-                # Try skipping one character (either left or right)
+                return False
+            l+=1
+            r-=1
+        return True
+
+    def validPalindrome(self, s: str) -> bool:
+        l, r = 0, len(s) - 1
+        while l<r:
+            if s[l] != s[r]:
                 return self.is_palindrome(s, l + 1, r) or self.is_palindrome(s, l, r - 1)
-            l += 1
-            r -= 1
-            
-        return True  # If no mismatch was found, it's already a palindrome
+            l+=1
+            r-=1
+        return True
