a2298260890eb2279acd56d5f58385c339598b28
AtharvaMusale <46114095+AtharvaMusale@users.noreply.github.com>
2025-01-31 09:00:26 -0500

    Time: 3 ms (91.91%) | Memory: 21.2 MB (66.20%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,21 +1,23 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        # What for empty array?
-        # Can I use heap?
-
-        count = {}
-        for num in nums:
-            count[num] = 1 + count.get(num,0)
+        count = defaultdict(int)
+        for i in nums:
+            if i in count:
+                count[i] += 1
+            else:
+                count[i] = 1
         
-        heap = []
-        for n in count.keys():
-            heapq.heappush(heap, (count[n],n))
-            if len(heap)>k:
-                heapq.heappop(heap)
+        # {1:3, 2:2, 3:1}
+        min_heap = []
+        for val,freq in count.items():
+            heapq.heappush(min_heap, (freq,val))
+            while len(min_heap) > k:
+                heapq.heappop(min_heap)
             
         
-        res = []
-        for i in range(k):
-            res.append(heapq.heappop(heap)[1])
-        return res
-
+        top_k = []
+        while min_heap:
+            freq,num = heapq.heappop(min_heap)
+            top_k.append(num)
+        
+        return top_k
