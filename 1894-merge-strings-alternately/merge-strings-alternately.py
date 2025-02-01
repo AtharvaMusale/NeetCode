2cf3626166e704eeab6ff1ae02cbc51f3d42e7d0
AtharvaMusale <46114095+AtharvaMusale@users.noreply.github.com>
2025-02-01 15:45:08 -0500

    Time: 39 ms (46.31%) | Memory: 17.7 MB (39.09%) - LeetSync

--- 1894-merge-strings-alternately/merge-strings-alternately.py
+++ 1894-merge-strings-alternately/merge-strings-alternately.py
@@ -1,37 +1,16 @@
-class Solution:
-    def mergeAlternately(self, word1: str, word2: str) -> str:
-#         p1,p2 = 0,0
-#         res = []
-#         while p1<len(word1) and p2<len(word2):
-#             res.append(word1[p1])
-#             res.append(word2[p2])
-#             p1+=1
-#             p2+=1
-
-#         while p1<len(word1):
-#             res.append(word1[p1])
-#             p1+=1
-        
-#         while p2 < len(word2):
-#             res.append(word2[p2])
-#             p2+=1
-        
-#         return "".join(res)
-
-# def mergeAlternately(word1: str, word2: str) -> str:
-            result = []
-            len1, len2 = len(word1), len(word2)
-            min_len = min(len1, len2)
-
-            # Append characters alternately
-            for i in range(min_len):
+class Solution(object):
+    def mergeAlternately(self, word1, word2):
+        """
+        :type word1: str
+        :type word2: str
+        :rtype: str
+        """
+        result = []
+        i = 0
+        while i < len(word1) or i < len(word2):
+            if i < len(word1):
                 result.append(word1[i])
+            if i < len(word2):
                 result.append(word2[i])
-
-            # Append the rest of the longer string
-            if len1 > min_len:
-                result.append(word1[min_len:])
-            if len2 > min_len:
-                result.append(word2[min_len:])
-
-            return ''.join(result)
+            i += 1
+        return ''.join(result)
