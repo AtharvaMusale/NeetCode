3552cc69a5693b78b03b056e3e3c3e1695c38828
AtharvaMusale <46114095+AtharvaMusale@users.noreply.github.com>
2025-02-01 10:35:47 -0500

    Time: 97 ms (49.73%) | Memory: 26.8 MB (61.63%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -1,18 +1,15 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        # Return profit
-        # Can there be negative prices? No
-        #  Duplicates? Maybe
-        
-        # Approach:
-        # [7,1,5,3,6,4] : l=3,r=6 profit = 7
+        l, r = 0,1
         maxP = 0
-        l,r = 0,1
+
         while r<len(prices):
-            if prices[l]<prices[r]:
+            if prices[l] < prices[r]:
                 profit = prices[r] - prices[l]
-                maxP = max(profit,maxP)
+                maxP = max(maxP, profit)
+            
             else:
                 l = r
-            r +=1
+            
+            r+=1
         return maxP
