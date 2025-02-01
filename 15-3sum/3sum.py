1fcc95654559f70653f9770b39180d7deb6668ba
AtharvaMusale <46114095+AtharvaMusale@users.noreply.github.com>
2025-02-01 11:09:07 -0500

    Time: 431 ms (92.99%) | Memory: 20.6 MB (58.19%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -1,29 +1,18 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        # Can there be negative numbers?
-        # Can there be duplicates?
-        # 
-
-        # Approach:
-        # i,j,k 3 ptr, Sort the input -> Fix One Pointer -> Use other two for Condition check
-
-        res = []
         nums.sort()
-
-        # [-4,-4,-1,0,1,2]
+        res = []
         for i in range(len(nums)-2):
-            #-4
             if nums[i]>0:
                 break
             
-            if i>0 and nums[i] == nums[i-1]:
+            if i > 0 and  nums[i] == nums[i-1]:
                 continue
-
+            
             l, r = i+1, len(nums)-1
-
             while l<r:
                 add = nums[i] + nums[l] + nums[r]
-                if add>0:
+                if add >0:
                     r-=1
                 elif add<0:
                     l+=1
@@ -32,6 +21,5 @@ class Solution:
                     l+=1
                     r-=1
                     while nums[l] == nums[l-1] and l<r:
-                        l+=1
+                        l +=1
         return res 
-
