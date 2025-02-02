3a1a66f07df4c9c1dbef6e1dfa0b59ef6de7b165
AtharvaMusale <46114095+AtharvaMusale@users.noreply.github.com>
2025-02-02 13:57:10 -0500

    Time: 12 ms (61.96%) | Memory: 19.1 MB (77.84%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.py
+++ 42-trapping-rain-water/trapping-rain-water.py
@@ -1,56 +1,42 @@
-# class Solution:
-#     def trap(self, nums: List[int]) -> int:
-#         # res = 0
-#         # l, r = 0, len(nums)-1
-#         # leftMax, rightMax = nums[l], nums[r]
-
-#         # while l<r:
-#         #     if leftMax<rightMax:
-#         #         l+=1
-#         #         leftMax = max(leftMax,nums[l])
-#         #         res += leftMax- nums[l]
-
-#         #     else:
-#         #         r-=1
-#         #         rightMax = max(rightMax, nums[r])
-#         #         res += rightMax- nums[r]
-
-#         # return res 
-
-#         res = 0
-#         l, r = 0, len(nums)-1
-#         leftMax, rightMax = nums[l], nums[r]
-
-#         while l<r:
-#             if leftMax<rightMax:
-#                 l+=1
-#                 leftMax = max(leftMax, nums[l])
-#                 res += leftMax- nums[l]
-            
-#             else:
-#                 r-=1
-#                 rightMax = max(rightMax, nums[r])
-#                 res+= rightMax - nums[r]
-#         return res
-
 class Solution:
-    def trap(self, nums: List[int]) -> int:
-        # Edge case: If the list has fewer than 3 elements, no water can be trapped
-        if len(nums) < 3:
+    def trap(self, height: List[int]) -> int:
+        # Zero or Positive 
+        # Width is always 1
+        # Trapped water?
+        # if not height:
+        #     return 0
+        
+        # l ,r = 0, len(height)-1
+        # leftMax,rightMax = height[l], height[r]
+        # res = 0
+        # while l<r:
+        #     if leftMax < rightMax:
+        #         l+=1
+        #         leftMax = max(leftMax,height[l])
+        #         res+= leftMax - height[l]
+        #     else:
+        #         r-=1
+        #         rightMax = max(rightMax,height[r])
+        #         res += rightMax - height[r]
+        
+        # return res
+
+        if not height:
             return 0
 
+        l, r = 0, len(height)-1
+        leftMax, rightMax = height[l], height[r]
         res = 0
-        l, r = 0, len(nums) - 1
-        left_max, right_max = nums[l], nums[r]
 
-        while l < r:
-            if left_max < right_max:
-                l += 1
-                left_max = max(left_max, nums[l])
-                res += left_max - nums[l]  # Add water trapped at index l
+        while l<r:
+            if leftMax<rightMax:
+                l+=1
+                leftMax = max(leftMax,height[l])
+                res += leftMax - height[l]
+
             else:
                 r -= 1
-                right_max = max(right_max, nums[r])
-                res += right_max - nums[r]  # Add water trapped at index r
+                rightMax = max(rightMax, height[r])
+                res += rightMax - height[r]
 
         return res
