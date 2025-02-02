41612cd0c091621618eb698e0bfc7f43abde50fa
AtharvaMusale <46114095+AtharvaMusale@users.noreply.github.com>
2025-02-02 09:55:59 -0500

    Time: 0 ms (100.00%) | Memory: 17.8 MB (51.82%) - LeetSync

--- 78-subsets/subsets.py
+++ 78-subsets/subsets.py
@@ -1,22 +1,17 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        #  Unique Elements 
-        # No dup any order
         res = []
         subset = []
 
-        def dfs(i):
-            if i >= len(nums):
+        def backtracking(i):
+            if i == len(nums):
                 res.append(subset.copy())
                 return
-            
+                
             subset.append(nums[i])
-            dfs(i+1)
+            backtracking(i+1)
             subset.pop()
-            dfs(i+1)
-        dfs(0)
+            backtracking(i+1)
+        
+        backtracking(0)
         return res
-
-            
-
-            
