3e7cd314b134ed23c27fba3110ef885a0b0a6902
AtharvaMusale <46114095+AtharvaMusale@users.noreply.github.com>
2025-02-02 13:44:14 -0500

    Time: 18 ms (60.91%) | Memory: 18 MB (27.48%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -1,7 +1,5 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        # [abcabcbb]
-        # l = a r = a # curr_len = r-l+1 # MaxLen = max(max_len, curr_len)
         l = 0
         hashset = set()
         res = 0
@@ -10,8 +8,9 @@ class Solution:
             while s[r] in hashset:
                 hashset.remove(s[l])
                 l+=1
-            hashset.add(s[r])
 
-            res = max(res,r-l+1)
-        
+
+            hashset.add(s[r])
+            res = max(res, r - l + 1)
+            
         return res
