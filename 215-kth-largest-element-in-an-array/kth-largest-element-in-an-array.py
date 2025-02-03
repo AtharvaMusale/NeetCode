52bcf28632a185e9091a294837f205f04b20fddf
AtharvaMusale <46114095+AtharvaMusale@users.noreply.github.com>
2025-02-03 15:54:23 -0500

    Time: 378 ms (5.01%) | Memory: 36 MB (6.43%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.py
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.py
@@ -1,11 +1,38 @@
+# class Solution:
+#     def findKthLargest(self, nums: List[int], k: int) -> int:
+#         min_heap = []
+
+#         for n in nums:
+#             heapq.heappush(min_heap,n)
+#             while len(min_heap) > k:
+#                 heapq.heappop(min_heap)
+            
+#         return min_heap[0]
+            
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
-        min_heap = []
+        k = len(nums) - k
 
-        for n in nums:
-            heapq.heappush(min_heap,n)
-            while len(min_heap) > k:
-                heapq.heappop(min_heap)
-            
-        return min_heap[0]
-            
+        def quickSelect(left, right):            
+            pivot = nums[right]
+            low = left
+            high = right
+
+            while low <= high:
+                while low <= high and nums[low] < pivot:
+                    low += 1
+                while low <= high and nums[high] > pivot:
+                    high -= 1
+                if low <= high:
+                    nums[low], nums[high] = nums[high], nums[low]
+                    low += 1
+                    high -= 1
+
+            if k <= high:
+                return quickSelect(left, high)
+            elif k >= low:
+                return quickSelect(low, right)
+            else:
+                return nums[k]
+
+        return quickSelect(0, len(nums) - 1)
