cfffa102f1ed9cb5940c1dba1ce90e602e9c059d
AtharvaMusale <46114095+AtharvaMusale@users.noreply.github.com>
2025-02-02 21:40:19 -0500

    Time: 159 ms (52.43%) | Memory: 18.9 MB (62.50%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.py
+++ 907-koko-eating-bananas/koko-eating-bananas.py
@@ -1,40 +1,21 @@
-# class Solution:
-#     def minEatingSpeed(self, piles: List[int], h: int) -> int:
-#         # [3,6,7,11] [1, 2, 2, 3]
-#         # HOURS = INT(PILE[I]/K) <= h if greater then increase k
-        
-#         l, r = 1 ,  max(piles)
-#         res = r
-
-#         while l<=r:
-#             k = l + (r-l)//2
-            
-#             total = 0
-#             for p in piles:
-#                 total += math.ceil(int(p)/k)
-
-#             if total > h:
-#                 l = k + 1
-#             else:
-#                 res = k
-#                 r = k - 1
-
-#         return res            
-
 class Solution:
+    def check_feasibile(self, piles, k, h):
+        hours = 0
+        for p in piles:
+            hours += math.ceil(p/k)
+        return hours<=h
+
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
+
         l, r = 1, max(piles)
-        res = r
+        ans = -1
+        while l<=r:
+            k = l + ((r-l)//2)
+            if self.check_feasibile(piles, k, h):
+                ans = k
+                r = k-1
 
-        while l <= r:
-            k = (l + r) // 2
-
-            totalTime = 0
-            for p in piles:
-                totalTime += math.ceil(float(p) / k)
-            if totalTime <= h:
-                res = k
-                r = k - 1
             else:
                 l = k + 1
-        return res
+        
+        return ans
