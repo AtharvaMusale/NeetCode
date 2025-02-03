76167471c5930fa6119ec6bffe02dfd05b829563
AtharvaMusale <46114095+AtharvaMusale@users.noreply.github.com>
2025-02-03 14:29:00 -0500

    Time: 7 ms (81.03%) | Memory: 18.3 MB (43.30%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -1,17 +1,20 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        # lowercase, removing all alphanumeric
+        #  Convert to lowercase
+        # l = r  while l<=r and each char is alphanumeric then compare and adjust   
+        l = 0
+        r = len(s)-1
         s = s.lower()
-        l, r = 0, len(s)-1
-        while l<=r:
+        while l< r:
             while l<=r and not s[l].isalnum():
                 l+=1
             while l<=r and not s[r].isalnum():
                 r-=1
             
-            if l<=r and s[l]!=s[r]:
+            if l<r and s[l]!=s[r]:
                 return False
-            
+
             l+=1
             r-=1
+
         return True
