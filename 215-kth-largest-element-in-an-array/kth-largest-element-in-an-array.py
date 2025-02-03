c919325581c7986e9756153bce84249dd5f8aff2
AtharvaMusale <46114095+AtharvaMusale@users.noreply.github.com>
2025-02-03 15:53:53 -0500

    Time: 103 ms (24.11%) | Memory: 28.7 MB (59.93%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.py
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.py
@@ -3,7 +3,9 @@ class Solution:
         min_heap = []
 
         for n in nums:
-            heapq.heappush(min_heap, n)
-            while len(min_heap)>k:
+            heapq.heappush(min_heap,n)
+            while len(min_heap) > k:
                 heapq.heappop(min_heap)
+            
         return min_heap[0]
+            
