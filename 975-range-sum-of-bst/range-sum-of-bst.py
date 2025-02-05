e7289048f94ae597f30a4a7d08f4c15584ca504e
AtharvaMusale <46114095+AtharvaMusale@users.noreply.github.com>
2025-02-05 17:10:05 -0500

    Time: 4 ms (49.07%) | Memory: 25.2 MB (18.86%) - LeetSync

--- 975-range-sum-of-bst/range-sum-of-bst.py
+++ 975-range-sum-of-bst/range-sum-of-bst.py
@@ -1,24 +1,24 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
 class Solution:
     def rangeSumBST(self, root: Optional[TreeNode], low: int, high: int) -> int:
-
         def dfs(node):
             if not node:
                 return 0
-            
-            if low <= node.val <= high:
-                return node.val + dfs(node.left) + dfs(node.right)
-            
-                
+
+            # If the current node's value is greater than the high value,
+            # explore the left subtree only, because all values in the right subtree
+            # will also be greater than 'high' and thus out of the required range.
+            if node.val > high:
+                return dfs(node.left)
+
+            # If the current node's value is less than the low value,
+            # explore the right subtree only, because all values in the left subtree
+            # will be less than 'low' and thus out of the required range.
+            elif node.val < low:
+                return dfs(node.right)
+
+            # If the current node's value is within the range,
+            # continue the search in both directions.
             else:
-                return dfs(node.left) + dfs(node.right)
+                return node.val + dfs(node.left) + dfs(node.right)
         
         return dfs(root)
-        
-
-        
