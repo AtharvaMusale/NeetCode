de6b49e38cb39f6c25e554496f9baca3f52a2efe
AtharvaMusale <46114095+AtharvaMusale@users.noreply.github.com>
2025-02-06 16:50:53 -0500

    Time: 3 ms (52.44%) | Memory: 19 MB (30.09%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,15 +1,10 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        # Are there Negative Numbers?
-        # Can there be multiple solutions in same array?
-
-        #Approach:
-        # [2,7,11,15] 9
         hashmap = {}
-        for i,num in enumerate(nums):
-            diff = target - num # 7
-            if diff in hashmap:
-                return [i, hashmap[diff]]
 
-            hashmap[num] = i
+        for ind,val in enumerate(nums):
+            diff = target - val
+            if diff in hashmap:
+                return [hashmap[diff],ind]
+            hashmap[val] = ind
         return 
