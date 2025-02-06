66136e1bae7e2877321a6e733fde7b376592f118
AtharvaMusale <46114095+AtharvaMusale@users.noreply.github.com>
2025-02-06 16:59:42 -0500

    Time: 0 ms (100.00%) | Memory: 20.9 MB (59.09%) - LeetSync

--- 56-merge-intervals/merge-intervals.py
+++ 56-merge-intervals/merge-intervals.py
@@ -1,17 +1,14 @@
 class Solution:
     def merge(self, nums: List[List[int]]) -> List[List[int]]:
+        res = []
         if not nums:
             return []
-        nums.sort(key = lambda x:x[0])
+        nums.sort(key= lambda x:x[0])
 
-        res = []
         for num in nums:
-            start, end = num
-        
+            start,end = num
             if not res or res[-1][1] < start:
                 res.append(num)
-            
-            else:
-                res[-1][1] = max(res[-1][1], num[1])
-        
+            else:   
+                res[-1][1] = max(res[-1][1],num[1])
         return res
