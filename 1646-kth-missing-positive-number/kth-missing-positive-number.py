06026d4b2f61976fdd3dc684254c1162e3833864
AtharvaMusale <46114095+AtharvaMusale@users.noreply.github.com>
2025-02-07 10:17:23 -0500

    Time: 0 ms (100.00%) | Memory: 18 MB (13.27%) - LeetSync

--- 1646-kth-missing-positive-number/kth-missing-positive-number.py
+++ 1646-kth-missing-positive-number/kth-missing-positive-number.py
@@ -1,26 +1,13 @@
-# class Solution:
-#     def findKthPositive(self, arr: List[int], k: int) -> int:
-#         miss = 0
-#         arr_max = max(arr)
-
-#         for i in range(1,arr_max):
-#             if i not in arr:
-#                 miss+=1
-        
-#         if k-miss>0:
-#             return arr_max + k - miss
 class Solution:
     def findKthPositive(self, arr: List[int], k: int) -> int:
-        left, right = 0, len(arr)
+        l = 0
+        r = len(arr)
 
-        while left < right:
-            mid = (left + right) // 2
-            # Check how many numbers are missing until arr[mid]
-            if arr[mid] - (mid + 1) < k:
-                left = mid + 1
+        while l<r:
+            m = (l+r)//2
+            if arr[m] - (m + 1) < k:
+                l = m + 1
             else:
-                right = mid
+                r = m
+        return l + k
 
-        # If we exhaust the list, then the answer is outside the max value in array
-        # (left) + k - the number of missing numbers before the last element in array
-        return left + k
