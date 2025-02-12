de487280dccb37caef28a0ccd43ee86ded54ba8f
AtharvaMusale <46114095+AtharvaMusale@users.noreply.github.com>
2025-02-12 15:06:32 -0500

    Time: 62 ms (98.36%) | Memory: 19.5 MB (92.66%) - LeetSync

--- 542-01-matrix/01-matrix.py
+++ 542-01-matrix/01-matrix.py
@@ -1,18 +1,35 @@
 class Solution:
     def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
-        n, m = len(mat), len(mat[0])
+        # n, m = len(mat), len(mat[0])
+
+        # for i in range(n):
+        #     for j in range(m):
+        #         if mat[i][j] != 0:
+        #             top = mat[i-1][j] if i >0  else (n + m -2)
+        #             left = mat[i][j-1] if j > 0 else (n+m-2)
+        #             mat[i][j] = min(top+1, left+1)
+        
+        # for i in range(n-1,-1,-1):
+        #     for j in range(m-1,-1,-1):
+        #         if mat[i][j]!= 0:
+        #             bot = mat[i+1][j] if i + 1 <n else n+m-2
+        #             right = mat[i][j+1] if j+1 <m else n+m-2
+        #             mat[i][j] = min(mat[i][j],bot+1,right+1)
+        # return mat
+
+        n,m = len(mat), len(mat[0])
 
         for i in range(n):
             for j in range(m):
-                if mat[i][j] != 0:
-                    top = mat[i-1][j] if i >0  else (n + m -2)
+                if mat[i][j]!=0:
+                    top = mat[i-1][j] if i > 0 else (n+m-2)
                     left = mat[i][j-1] if j > 0 else (n+m-2)
-                    mat[i][j] = min(top+1, left+1)
-        
+                    mat[i][j] = min(top+1,left+1)
+            
         for i in range(n-1,-1,-1):
             for j in range(m-1,-1,-1):
-                if mat[i][j]!= 0:
-                    bot = mat[i+1][j] if i + 1 <n else n+m-2
-                    right = mat[i][j+1] if j+1 <m else n+m-2
-                    mat[i][j] = min(mat[i][j],bot+1,right+1)
+                if mat[i][j]!=0:
+                    bot = mat[i+1][j] if i+1 < n else (n+m-2)
+                    right = mat[i][j+1] if j+1 < m else (n+m-2)
+                    mat[i][j] = min(mat[i][j], bot + 1, right +1)
         return mat
