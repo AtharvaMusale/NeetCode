fb05312faf23a21f510f7f0f28fb24076b889639
AtharvaMusale <46114095+AtharvaMusale@users.noreply.github.com>
2025-02-14 08:06:33 -0500

    Time: 14 ms (17.14%) | Memory: 18.1 MB (52.28%) - LeetSync

--- 605-can-place-flowers/can-place-flowers.py
+++ 605-can-place-flowers/can-place-flowers.py
@@ -1,9 +1,22 @@
+# class Solution:
+#     def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
+#         bed = [0] + flowerbed + [0]
+#         for i in range(1,len(bed)-1):
+#             if bed[i] == bed[i-1] == bed[i+1] == 0:
+#                 n-=1
+#                 bed[i] = 1
+            
+#         return n<=0
+
 class Solution:
     def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
-        bed = [0] + flowerbed + [0]
-        for i in range(1,len(bed)-1):
-            if bed[i] == bed[i-1] == bed[i+1] == 0:
-                n-=1
-                bed[i] = 1
-            
-        return n<=0
+    
+        for i in range(len(flowerbed)):
+            left = i == 0 or flowerbed[i-1] == 0
+            right = i == len(flowerbed) - 1 or flowerbed[i+1] == 0
+
+            if left and right and flowerbed[i] == 0:
+                flowerbed[i] = 1
+                n -= 1
+        
+        return n <= 0
