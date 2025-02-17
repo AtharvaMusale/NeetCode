830c5038a7e5c3670c0f1800ef72bd7b3de75abe
AtharvaMusale <46114095+AtharvaMusale@users.noreply.github.com>
2025-02-17 13:23:09 -0500

    Time: 3 ms (37.29%) | Memory: 17.9 MB (29.19%) - LeetSync

--- 71-simplify-path/simplify-path.py
+++ 71-simplify-path/simplify-path.py
@@ -1,16 +1,11 @@
 class Solution:
     def simplifyPath(self, path: str) -> str:
-        #  single . -> current directory
-        # .. -> previous directory
-        # ////... -> /
-        # ..... -> .
         stack = []
         path = path.split("/")
-        # home, / , foo
         for c in path:
             if c == "..":
                 if stack:
                     stack.pop()
-            elif c and c != ".":
+            elif c and c!=".":
                 stack.append(c)
         return "/" + "/".join(stack)
