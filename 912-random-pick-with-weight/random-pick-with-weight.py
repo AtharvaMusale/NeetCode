c70821276d1b3599b3f6b5d1976e6d6281ae0e3f
AtharvaMusale <46114095+AtharvaMusale@users.noreply.github.com>
2025-02-17 09:48:19 -0500

    Time: 61 ms (78.79%) | Memory: 23.4 MB (45.71%) - LeetSync

--- 912-random-pick-with-weight/random-pick-with-weight.py
+++ 912-random-pick-with-weight/random-pick-with-weight.py
@@ -1,28 +1,27 @@
-import random
-
 class Solution:
-    def __init__(self, w):
-        self.cum_sum = []
-        current_sum = 0
-        for weight in w:
-            current_sum += weight
-            self.cum_sum.append(current_sum)
 
-    def pickIndex(self):
-        # Generate a random number between 1 and the total sum of weights
-        u = random.randint(1, self.cum_sum[-1])
+    def __init__(self, w: List[int]):
+        self.cum_sum = []
+        weight = 0
+        for wt in w:
+            weight += wt
+            self.cum_sum.append(weight)
+
+
+    def pickIndex(self) -> int:
         
-        # Binary search for the smallest index j where cum_sum[j] >= u
-        l, r = 0, len(self.cum_sum) - 1
-        while l <= r:
-            m = (l + r) // 2
-            if self.cum_sum[m] >= u:
-                r = m - 1
+        u = random.randint(1,self.cum_sum[-1])
+        p = 0
+        l,r  = 0, len(self.cum_sum)-1
+        while l<=r:
+            m = (l+r)//2
+            if self.cum_sum[m]>=u:
+                r = m -1 
+                p = m
             else:
                 l = m + 1
-        return l
+        return p
 
-# Example of usage
-# w = [1, 3, 2]  # Example weights
+# Your Solution object will be instantiated and called as such:
 # obj = Solution(w)
-# print(obj.pickIndex())  # Outputs an index, where index 1 has the highest probability of being picked
+# param_1 = obj.pickIndex()
