680555bed67a6993c4b29ed6b01fd765abe312c5
AtharvaMusale <46114095+AtharvaMusale@users.noreply.github.com>
2025-02-20 15:23:21 -0500

    Time: 85 ms (85.81%) | Memory: 27.5 MB (67.72%) - LeetSync

--- 739-daily-temperatures/daily-temperatures.py
+++ 739-daily-temperatures/daily-temperatures.py
@@ -1,14 +1,12 @@
 class Solution:
-    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
-        # [73,74,74,71,69,72,76,73]
-        stack = []
-        res = [0] * len(temperatures)
+    def dailyTemperatures(self, temp: List[int]) -> List[int]:
+        res = [0] * len(temp)  # Result array of the same length as temp, initialized with 0s
+        stack = []  # Stack to store indices of temp array
 
-        for i,val in enumerate(temperatures):
-            while stack and val> stack[-1][0]:
-                stackT, stackInd = stack.pop()
-                res[stackInd] = i - stackInd
-            
-            stack.append((val,i))
-        
+        for i in range(len(temp)):
+            while stack and temp[i] > temp[stack[-1]]:
+                index = stack.pop()
+                res[index] = i - index  # Calculate the number of days until a warmer temperature
+            stack.append(i)  # Push current day's index onto the stack
+
         return res
