6df50edeacd1d89ba54b1c3357a2e505d4b5b43d
AtharvaMusale <46114095+AtharvaMusale@users.noreply.github.com>
2025-02-20 10:20:54 -0500

    Time: 3 ms (81.85%) | Memory: 18.5 MB (59.65%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
@@ -1,21 +1,14 @@
 class Solution:
-    def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        # Can there be negative numbers
-        # Can there be multiple solutions
-
-        l = 0
-        r = len(numbers)-1
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        l,r = 0, len(nums)-1
 
         while l<r:
-            add = numbers[l] + numbers[r]
+            add = nums[l] + nums[r]
 
             if add>target:
                 r-=1
-                
             elif add<target:
                 l+=1
-            
             else:
                 return [l+1,r+1]
-            
-        return []
+        
