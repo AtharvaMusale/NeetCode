6d5334f3e9bbc25b4f403b04e9f100cfe528e030
AtharvaMusale <46114095+AtharvaMusale@users.noreply.github.com>
2025-02-20 14:05:14 -0500

    Time: 116 ms (72.44%) | Memory: 18 MB (48.42%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
@@ -1,20 +1,15 @@
-from collections import defaultdict
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        char_count = defaultdict(int)
-        window_size = 0
-        max_len = 0
-        max_freq = 0
         l = 0
-
+        maxf= 0
+        res = 0
+        hashmap = {}
         for r in range(len(s)):
-            char_count[s[r]]+=1
-            max_freq = max(max_freq, char_count[s[r]])
-            window_size = r-l+1
-            if (window_size-max_freq)>k:
-                char_count[s[l]]-=1
+            hashmap[s[r]] = 1 + hashmap.get(s[r],0)
+            maxf = max(maxf,hashmap[s[r]])
+            while (r-l+1) - maxf >k:
+                hashmap[s[l]]-=1
                 l+=1
-            
-            max_len = max(max_len,r-l+1)
         
-        return max_len
+            res = max(res,r-l+1)
+        return res
