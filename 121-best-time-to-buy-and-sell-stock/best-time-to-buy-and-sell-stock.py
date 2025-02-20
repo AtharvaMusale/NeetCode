ee5d46553a366ecec1918875b3592fe61e455a43
AtharvaMusale <46114095+AtharvaMusale@users.noreply.github.com>
2025-02-20 11:13:20 -0500

    Time: 120 ms (35.57%) | Memory: 26.8 MB (77.78%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -1,12 +1,13 @@
 class Solution:
     def maxProfit(self, nums: List[int]) -> int:
-        l,r = 0,1
+        l, r = 0, 1
         maxP = 0
         while r<len(nums):
-            if nums[l] < nums[r]:
-                profit = nums[r] - nums[l]
-                maxP = max(maxP, profit)
-            else:
+            if nums[r] < nums[l]:
                 l = r
+            else:
+                profit = (nums[r]-nums[l])
+
+                maxP = max(maxP,profit)
             r+=1
-        return maxP 
+        return maxP
