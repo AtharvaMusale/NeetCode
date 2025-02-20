c62b6ba0a52d9d0b622e9f0b34272a970dccdf04
AtharvaMusale <46114095+AtharvaMusale@users.noreply.github.com>
2025-02-20 09:00:43 -0500

    Time: 5 ms (63.31%) | Memory: 21 MB (89.86%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,21 +1,14 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        hashmap = defaultdict(int)
+        count = {}
         for n in nums:
-            if n in hashmap:
-                hashmap[n] += 1
+            count[n] = 1+ count.get(n,0)
 
-            else:
-                hashmap[n] = 1
-            
-        # 1:3, 2:2, 3:1
         min_heap = []
-        for val,freq in hashmap.items():
-            heapq.heappush(min_heap, (freq,val))
+        for val,count in count.items():
+            heapq.heappush(min_heap, (count,val))
             if len(min_heap)>k:
                 heapq.heappop(min_heap)
         
-        top_k = []
-        while min_heap:
-            top_k.append(heapq.heappop(min_heap)[1])
-        return top_k
+        # Extract the elements from the heap
+        return [val for cnt, val in min_heap]
