ee861f9b11441f5bd2160c0b5a4752881572995d
AtharvaMusale <46114095+AtharvaMusale@users.noreply.github.com>
2025-02-21 09:01:41 -0500

    Time: 0 ms (100.00%) | Memory: 18.9 MB (27.74%) - LeetSync

--- 792-binary-search/binary-search.py
+++ 792-binary-search/binary-search.py
@@ -3,10 +3,9 @@ class Solution:
         l, r = 0, len(nums)-1
 
         while l<=r:
-            m = l + (r-l)//2
-
+            m = (l+r)//2
             if nums[m] > target:
-                r = m -1
+                r = m - 1
             elif nums[m] < target:
                 l = m + 1
             else:
