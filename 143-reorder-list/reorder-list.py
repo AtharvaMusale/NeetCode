1f3b8644e35affca96d794b08f1aad281312cf6b
AtharvaMusale <46114095+AtharvaMusale@users.noreply.github.com>
2025-02-21 14:01:39 -0500

    Time: 0 ms (100.00%) | Memory: 23.3 MB (38.21%) - LeetSync

--- 143-reorder-list/reorder-list.py
+++ 143-reorder-list/reorder-list.py
@@ -1,3 +1,36 @@
+# # Definition for singly-linked list.
+# # class ListNode:
+# #     def __init__(self, val=0, next=None):
+# #         self.val = val
+# #         self.next = next
+# class Solution:
+#     def reorderList(self, head: Optional[ListNode]) -> None:
+#         """
+#         Do not return anything, modify head in-place instead.
+#         """
+#         slow = fast = head
+#         while fast and fast.next:
+#             slow = slow.next
+#             fast = fast.next.next
+
+#         second = slow.next
+#         prev = slow.next = None
+
+#         while second:
+#             tmp = second.next
+#             second.next = prev
+#             prev = second
+#             second = tmp
+        
+#         first, second = head, prev
+#         while second:   
+#             tmp1, tmp2 = first.next, second.next
+#             first.next = second
+#             second.next = tmp1
+#             first, second = tmp1, tmp2
+
+
+
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, val=0, next=None):
@@ -8,27 +41,22 @@ class Solution:
         """
         Do not return anything, modify head in-place instead.
         """
-        slow = fast = head
+        slow, fast = head, head
+
         while fast and fast.next:
             slow = slow.next
             fast = fast.next.next
-            
-        second = slow.next
-        prev = slow.next = None
-
-        while second:
-            tmp = second.next
-            second.next = prev
-            prev = second
-            second = tmp
         
-        first, second = head, prev
-        while second:   
-            tmp1, tmp2 = first.next, second.next
-            first.next = second
-            second.next = tmp1
-            first, second = tmp1, tmp2
+        prev = None
+        cur = slow
 
-
-
+        while cur:
+            cur.next, prev, cur = prev, cur, cur.next
         
+        first = head
+        sec = prev
+        while sec.next:
+            first.next, first =  sec, first.next 
+            sec.next, sec = first, sec.next
+
+        return head
