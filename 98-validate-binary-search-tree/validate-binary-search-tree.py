6d8024a19a8891bb564413fed7e5d0118a8ccb9a
AtharvaMusale <46114095+AtharvaMusale@users.noreply.github.com>
2025-02-21 15:42:55 -0500

    Time: 3 ms (35.63%) | Memory: 19.8 MB (30.41%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.py
+++ 98-validate-binary-search-tree/validate-binary-search-tree.py
@@ -6,14 +6,13 @@
 #         self.right = right
 class Solution:
     def isValidBST(self, root: Optional[TreeNode]) -> bool:
-        def is_valid(node, min_val, max_val):
-            # An empty tree is a valid BST
+        def dfs(node, min_val, max_val):
             if not node:
                 return True
-            # Check if the current node value is within the valid range
+            
             if not (min_val < node.val < max_val):
                 return False
-            # Recursively validate the left and right subtrees
-            return is_valid(node.left, min_val, node.val) and is_valid(node.right, node.val, max_val)
-        
-        return is_valid(root, float('-inf'), float('inf'))
+            
+            return dfs(node.left, min_val, node.val) and dfs(node.right, node.val, max_val)
+        return dfs(root, float("-inf"), float("inf"))
+            
