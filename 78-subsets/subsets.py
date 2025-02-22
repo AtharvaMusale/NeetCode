9f333b3679b102825f7105caa7113da36cac185f
AtharvaMusale <46114095+AtharvaMusale@users.noreply.github.com>
2025-02-22 15:13:20 -0500

    Time: 0 ms (100.00%) | Memory: 17.8 MB (65.35%) - LeetSync

--- 78-subsets/subsets.py
+++ 78-subsets/subsets.py
@@ -1,17 +1,22 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
         res = []
-        subset = []
+        curr = []
 
-        def backtracking(i):
-            if i == len(nums):
-                res.append(subset.copy())
-                return
-                
-            subset.append(nums[i])
-            backtracking(i+1)
-            subset.pop()
-            backtracking(i+1)
+        def dfs(i, curr):
         
-        backtracking(0)
+            if i == len(nums):
+                res.append(curr[:])
+                return 
+    
+            # Recursive Exploration
+            curr.append(nums[i])
+
+            dfs(i+1, curr)
+
+            # Check feasibility
+            curr.pop()
+            dfs(i+1,curr)
+
+        dfs(0,[])
         return res
