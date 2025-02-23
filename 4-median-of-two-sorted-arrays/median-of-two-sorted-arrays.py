e80daeafd2e205e32c25513edd93fc4bb4a0e331
AtharvaMusale <46114095+AtharvaMusale@users.noreply.github.com>
2025-02-23 02:22:33 -0500

    Time: 2 ms (59.44%) | Memory: 17.9 MB (83.68%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.py
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.py
@@ -1,50 +1,14 @@
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        p1 = p2 = 0
-        merge = []
-        while p1 < len(nums1) and p2< len(nums2):
-            if nums1[p1] <= nums2[p2]:
-                merge.append(nums1[p1])
-                p1+=1
-            else:
-                merge.append(nums2[p2])
-                p2+=1
-            
-        while p1<len(nums1):
-            merge.append(nums1[p1])
-            p1+=1
+        m=len(nums1)
+        n=len(nums2)
+        l=nums1+nums2
+        l.sort()
+        if (n+m)%2==1:
+            return l[((n+m)//2)]
+        else:
+            median=(l[(n+m)//2]+l[((n+m)//2)-1])/2
+
+            return median
+
         
-        while p2< len(nums2):
-            merge.append(nums2[p2])
-            p2+=1
-        
-        n = len(merge)
-        if n%2!=0:
-            return float(merge[int(n/2)])
-
-        return float((merge[(n-1)//2] + merge[n//2])/2.0)
-
-        # p1 = 0
-        # p2 = 0
-        # merge = []
-        # while p1<len(nums1) and p2<len(nums2):
-        #     if nums1[p1]<=nums2[p2]:
-        #         merge.append(nums1[p1])
-        #         p1+=1
-        #     else:
-        #         merge.append(nums2[p2])
-        #         p2+=1
-            
-        # while p1<len(nums1):
-        #     merge.append(nums1[p1])
-        #     p1+=1
-
-        # while p2<len(nums2):    
-        #     merge.append(nums2[p2])
-        #     p2+=1
-
-        # n = len(merge)
-        # if n % 2 != 0:
-        #     return float(merge[int(n/2)])
-    
-        # return float((merge[(n - 1) // 2] + merge[n // 2]) / 2.0)
