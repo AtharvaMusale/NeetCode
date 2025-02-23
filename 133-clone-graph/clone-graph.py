c67b8e00136e1104c3f1dcaf15e12b79555d2b53
AtharvaMusale <46114095+AtharvaMusale@users.noreply.github.com>
2025-02-23 15:51:43 -0500

    Time: 44 ms (52.03%) | Memory: 18 MB (82.65%) - LeetSync

--- 133-clone-graph/clone-graph.py
+++ 133-clone-graph/clone-graph.py
@@ -10,17 +10,36 @@ from typing import Optional
 class Solution:
     def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
         
+        # if not node:
+        #     return None
+        
+        # oldToNew = {}
+        # copy = Node(node.val)
+        # oldToNew[node] = copy
+    
+        # q = deque([node])
+        # while q:
+        #     curr = q.popleft()
+
+        #     for nei in curr.neighbors:
+        #         if nei not in oldToNew:
+        #             copyNei = Node(nei.val)
+        #             oldToNew[nei] = copyNei
+        #             q.append(nei)
+        #         oldToNew[curr].neighbors.append(oldToNew[nei])
+        # return oldToNew[node]
+
+
         if not node:
             return None
         
         oldToNew = {}
         copy = Node(node.val)
         oldToNew[node] = copy
-    
+
         q = deque([node])
         while q:
             curr = q.popleft()
-
             for nei in curr.neighbors:
                 if nei not in oldToNew:
                     copyNei = Node(nei.val)
