f71a9730c8ea2953bfe9ca278d18e589217110dd
AtharvaMusale <46114095+AtharvaMusale@users.noreply.github.com>
2025-02-24 14:44:40 -0500

    Time: 4 ms (21.37%) | Memory: 19.7 MB (61.68%) - LeetSync

--- 57-insert-interval/insert-interval.py
+++ 57-insert-interval/insert-interval.py
@@ -1,31 +1,25 @@
+from typing import List
+
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
-        # res = []
-        # for i in range(len(intervals)):
-        #     if intervals[i][0] > newInterval[1]:
-        #         res.append(newInterval)
-        #         return res + intervals[i:]
-            
-        #     elif intervals[i][1] < newInterval[0]:
-        #         res.append(intervals[i])
-            
-        #     else:
-        #         newInterval = [min(newInterval[0],intervals[i][0]),max(newInterval[1], intervals[i][1])]
-            
-        # res.append(newInterval)
-    
-        # return res
+        # First, we need to insert the newInterval into intervals, then sort the entire list
+        intervals.append(newInterval)
+        intervals.sort(key=lambda x: x[0])
 
-        res = []
-        for i in range(len(intervals)):
-            if intervals[i][0] > newInterval[1]:
-                res.append(newInterval)
-                return res + intervals[i:]
-            elif intervals[i][1] < newInterval[0]:
-                res.append(intervals[i])
+        # Initialize the result list with the first interval
+        res = [intervals[0]]
 
+        # Iterate through the sorted intervals starting from the second interval
+        for i in range(1, len(intervals)):
+            currStart, currEnd = intervals[i]
+            lastEnd = res[-1][1]
+
+            # Check if the current interval overlaps with the last interval in res
+            if currStart <= lastEnd:
+                # If they overlap, merge the current interval into the last interval of res
+                res[-1][1] = max(lastEnd, currEnd)
             else:
-                newInterval = [min(intervals[i][0],newInterval[0]),max(newInterval[1],intervals[i][1])]
-            
-        res.append(newInterval)
+                # If they do not overlap, add the current interval to res
+                res.append([currStart, currEnd])
+
         return res
