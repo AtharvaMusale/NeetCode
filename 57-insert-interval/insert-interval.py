10d69893525971ca08a0ad61b8d600a089a4a9e1
AtharvaMusale <46114095+AtharvaMusale@users.noreply.github.com>
2025-02-24 14:56:20 -0500

    Time: 0 ms (100.00%) | Memory: 19.8 MB (41.74%) - LeetSync

--- 57-insert-interval/insert-interval.py
+++ 57-insert-interval/insert-interval.py
@@ -1,27 +1,20 @@
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
- 
-    
-        result = []
-        new_start, new_end = newInterval
-        
-        for index in range(len(intervals)):
-            start, end = intervals[index]
+        res = []
+        newStart, newEnd = newInterval
+        for i in range(len(intervals)):
+            start, end = intervals[i]
 
-            # If the current interval ends before the new interval starts, add it to the result
-            if end < new_start:
-                result.append([start, end])
+            if end < newStart:
+                res.append([start,end])
             
-            # If the current interval starts after the new interval ends, merge all remaining
-            elif start > new_end:
-                result.append([new_start, new_end])
-                return result + intervals[index:]
+            elif start>newEnd:
+                res.append([newStart,newEnd])
+                return res + intervals[i:]
             
-            # Overlapping intervals, adjust the new_start and new_end
             else:
-                new_start = min(start, new_start)
-                new_end = max(end, new_end)
-        
-        # Add the final merged interval if it wasn't added in the loop
-        result.append([new_start, new_end])
-        return result
+                newStart = min(start, newStart)
+                newEnd = max(end, newEnd)
+            
+        res.append([newStart,newEnd])
+        return res
