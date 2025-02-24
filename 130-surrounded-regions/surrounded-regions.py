963cddafc34c1047d4f2237a7a09fd3e5f99ae5c
AtharvaMusale <46114095+AtharvaMusale@users.noreply.github.com>
2025-02-24 10:07:15 -0500

    Time: 11 ms (31.50%) | Memory: 21.4 MB (97.42%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.py
+++ 130-surrounded-regions/surrounded-regions.py
@@ -1,34 +1,34 @@
 class Solution:
-    def solve(self, grid: List[List[str]]) -> None:
+    def solve(self, board: List[List[str]]) -> None:
         """
-        Do not return anything, modify grid in-place instead.
+        Do not return anything, modify board in-place instead.
         """
-        ROWS, COLS = len(grid),len(grid[0])
+        
+        ROWS, COLS = len(board), len(board[0])
 
         def dfs(r,c):
-            if r<0 or r>=ROWS or c<0 or c>=COLS or grid[r][c]!="O":
+            if r<0 or r>=ROWS or c <0 or c>=COLS or board[r][c] != "O":
                 return 
-            grid[r][c] = "T"
+            board[r][c] = "T"
             dfs(r+1,c)
             dfs(r-1,c)
             dfs(r,c+1)
             dfs(r,c-1)
+
+        for r in range(ROWS):
+            for c in range(COLS):
+                if r in [0,ROWS-1] or c in [0,COLS-1]:
+                    if board[r][c] =="O":
+                        dfs(r,c)
+
+        for r in range(ROWS):
+            for c in range(COLS):
+                if board[r][c] == "O":
+                    board[r][c] = "X"
         
         for r in range(ROWS):
             for c in range(COLS):
-                if grid[r][c] == "O" and r in [0,ROWS-1] or c in [0,COLS-1]:
-                    dfs(r,c)
+                if board[r][c] == "T":
+                    board[r][c]= "O"
         
-        for r in range(ROWS):
-            for c in range(COLS):
-                if grid[r][c]=="O":
-                    grid[r][c] = "X"
-        
-        for r in range(ROWS):
-            for c in range(COLS):
-                if grid[r][c]=="T":
-                    grid[r][c] = "O"
-
-
-
-
+        # return board
