dc1fabc98998b6c663b17aef7ed2c9360e47a30a
AtharvaMusale <46114095+AtharvaMusale@users.noreply.github.com>
2025-02-25 10:33:41 -0500

    Time: 405 ms (22.90%) | Memory: 19.5 MB (92.88%) - LeetSync

--- 744-network-delay-time/network-delay-time.py
+++ 744-network-delay-time/network-delay-time.py
@@ -1,56 +1,23 @@
-# import collections
-# import heapq
-# from typing import List
-
-# class Solution:
-#     def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
-#         edges = collections.defaultdict(list)
-#         for u, v, w in times:
-#             edges[u].append([v, w])
-        
-#         minHeap = [(0, k)]
-#         visit = set()
-#         t = 0
-#         while minHeap:
-#             w1, n1 = heapq.heappop(minHeap)
-#             if n1 in visit:
-#                 continue
-            
-#             visit.add(n1)
-#             t = max(t, w1)
-
-#             for w2, n2 in edges[n1]:
-#                 if n2 not in visit:
-#                     heapq.heappush(minHeap, (w1 + w2, n2))
-        
-#         return t if len(visit) == n else -1
-
-# # Example usage:
-# # solution = Solution()
-# # times = [[2, 1, 1], [2, 3, 1], [3, 4, 1]]
-# # n = 4
-# # k = 2
-# # print(solution.networkDelayTime(times, n, k))  # Output: 2
-
-
 class Solution:
     def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
-        edges = collections.defaultdict(list)
-        for u, v, w in times:
-            edges[u].append((v, w))
-
-        minHeap = [(0, k)]
-        visit = set()
+        hashmap = defaultdict(list)
+        for u,v,w in times:
+            hashmap[u].append((v,w))
+        
+        
+        minHeap = [(0,k)]
         t = 0
+        visit = set()
+
         while minHeap:
             w1, n1 = heapq.heappop(minHeap)
             if n1 in visit:
                 continue
             visit.add(n1)
-            t = w1
+            t = max(t, w1)
 
-            for n2, w2 in edges[n1]:
+            for n2, w2 in hashmap[n1]:
                 if n2 not in visit:
-                    heapq.heappush(minHeap, (w1 + w2, n2))
+                    heapq.heappush(minHeap, (w1+w2, n2))
+        
         return t if len(visit) == n else -1
-
