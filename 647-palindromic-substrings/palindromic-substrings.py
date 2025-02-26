02d49f977a5fb188ecdd810035272c0090027897
AtharvaMusale <46114095+AtharvaMusale@users.noreply.github.com>
2025-02-26 12:34:01 -0500

    Time: 130 ms (63.72%) | Memory: 17.7 MB (87.39%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.py
+++ 647-palindromic-substrings/palindromic-substrings.py
@@ -1,17 +1,17 @@
 class Solution:
     def countSubstrings(self, s: str) -> int:
-        def expandCentre(l,r):
-            count = 0
-            while l>=0 and r<len(s) and s[l] == s[r]:
-                count+=1
+
+        res = 0
+        for i in range(len(s)):
+            l, r= i,i
+            while l>=0 and r<len(s) and s[r]==s[l]:
+                res+=1
                 l-=1
                 r+=1
-            return count
-
-        
-
-        total = 0
-        for i in range(len(s)):
-            total += expandCentre(i,i)
-            total+= expandCentre(i,i+1)
-        return total
+            
+            l, r= i,i+1
+            while l>=0 and r<len(s) and s[r]==s[l]:
+                res+=1
+                l-=1
+                r+=1
+        return res
