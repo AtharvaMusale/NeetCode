2332c5869f6facf0c09d2ca3a851dc087c18ef4b
AtharvaMusale <46114095+AtharvaMusale@users.noreply.github.com>
2025-02-28 11:15:17 -0500

    Time: 1033 ms (8.62%) | Memory: 248 MB (6.11%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.py
+++ 115-distinct-subsequences/distinct-subsequences.py
@@ -1,77 +1,21 @@
-# class Solution:
-#     def numDistinct(self, s: str, t: str) -> int:
-#         dp = {}
-#         def dfs(i,j):
-#             if j == len(t):
-#                 return 1
-#             if i == len(s):
-#                 return 0
-
-#             if (i,j) in dp:
-#                 return dp[(i,j)]
-
-#             if s[i]==t[j]:
-#                 dp[(i,j)] = dfs(i+1,j+1) + dfs(i+i,j)
-#             else:
-#                 dp[(i,j)] =  dfs(i+i,j)
-#             return dp[(i,j)]
-
-#         return dfs(0,0)
-# class Solution:
-#     def numDistinct(self, s: str, t: str) -> int:
-#         dp = {}
-
-#         def dfs(i, j):
-#             if j == len(t):
-#                 return 1
-#             if i == len(s):
-#                 return 0
-
-#             if (i, j) in dp:
-#                 return dp[(i, j)]
-
-#             if s[i] == t[j]:
-#                 dp[(i, j)] = dfs(i + 1, j + 1) + dfs(i + 1, j)
-#             else:
-#                 dp[(i, j)] = dfs(i + 1, j)
-            
-#             return dp[(i, j)]
-
-#         return dfs(0, 0)
-            
-
 class Solution:
     def numDistinct(self, s: str, t: str) -> int:
-        cache = {}
-
-        for i in range(len(s) + 1):
-            cache[(i, len(t))] = 1
-        for j in range(len(t)):
-            cache[(len(s), j)] = 0
-
-        for i in range(len(s) - 1, -1, -1):
-            for j in range(len(t) - 1, -1, -1):
-                if s[i] == t[j]:
-                    cache[(i, j)] = cache[(i + 1, j + 1)] + cache[(i + 1, j)]
-                else:
-                    cache[(i, j)] = cache[(i + 1, j)]
-        return cache[(0, 0)]
-
-        # dp = {}
-        # m, n = len(s), len(t)
-        # def findString(i, j):
-        #     if (i, j) in dp: return dp[(i, j)]
-        #     if j >= n:
-        #         return 1 # complted entire t 
-        #     if i>=m :
-        #         return 0
-        #     if s[i] == t[j]:
-        #         dp[(i,j)] = findString(i+1, j+1)+ findString(i+1, j) # conside s[i] as match and move ahead to match next char in t or try to find another match to t[j] in s[i+1:]
-        #     else:
-        #         dp[(i,j)]=  findString(i+1, j)
-        #     return dp[(i,j)]
-        # return findString(0,0)
+        if len(t) > len(s):
+            return 0
         
+        dp = {}
+        def dfs(i, j):
+            if j == len(t):
+                return 1
+            if i == len(s):
+                return 0
+            if (i, j) in dp:
+                return dp[(i, j)]
+            
+            res = dfs(i + 1, j)
+            if s[i] == t[j]:
+                res += dfs(i + 1, j + 1)
+            dp[(i, j)] = res
+            return res
 
-
-        
+        return dfs(0, 0)
