e0f0df295eb80965b4c1537e177d11c348a9ee59
AtharvaMusale <46114095+AtharvaMusale@users.noreply.github.com>
2025-03-07 21:39:47 -0500

    Time: 35 ms (56.90%) | Memory: 19.2 MB (80.42%) - LeetSync

--- 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.py
+++ 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.py
@@ -1,28 +1,29 @@
 class Solution:
-    def pacificAtlantic(self, grid: List[List[int]]) -> List[List[int]]:
-        pac, atl = set(), set()
-        ROWS, COLS = len(grid), len(grid[0])
+    def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
+        ROWS, COLS = len(heights), len(heights[0])
+        atl, pac = set(), set()
         res = []
+
         def dfs(r,c,visit,prevHeight):
-            if r<0 or c<0 or r>=ROWS or c>=COLS or (r,c) in visit or grid[r][c]<prevHeight:
+            if r<0 or c<0 or r>=ROWS or c>=COLS or (r,c) in visit or heights[r][c]<prevHeight:
                 return 
             visit.add((r,c))
-            dfs(r+1,c,visit,grid[r][c])
-            dfs(r-1,c,visit,grid[r][c])
-            dfs(r,c+1,visit,grid[r][c])
-            dfs(r,c-1,visit,grid[r][c])
-
-        for c in range(COLS):
-            dfs(0,c,pac,grid[0][c]) 
-            dfs(ROWS-1, c, atl, grid[ROWS-1][c])
-        
+            dfs(r+1,c,visit,heights[r][c])
+            dfs(r-1,c,visit,heights[r][c])
+            dfs(r,c+1,visit,heights[r][c])
+            dfs(r,c-1,visit,heights[r][c])
+            
         for r in range(ROWS):
-            dfs(r, 0, pac, grid[r][0])
-            dfs(r, COLS-1, atl, grid[r][COLS-1])
-
+            dfs(r,0,pac,heights[r][0])
+            dfs(r,COLS-1,atl,heights[r][COLS-1])
+        
+        for c in range(COLS):
+            dfs(0, c,pac,heights[0][c])
+            dfs(ROWS-1, c, atl, heights[ROWS-1][c])
+        
         for r in range(ROWS):
             for c in range(COLS):
                 if (r,c) in pac and (r,c) in atl:
                     res.append([r,c])
-                
+        
         return res
