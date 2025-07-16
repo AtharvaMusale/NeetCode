29deb100cf8cf20eabd9efd7518c5f22c6b232b7
AtharvaMusale <46114095+AtharvaMusale@users.noreply.github.com>
2025-07-16 10:39:52 -0400

    Time: 15 ms (41.90%) | Memory: 18 MB (49.34%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,10 +1,9 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        charS, charT = {}, {}
-        if len(s) != len(t):
-            return False
-        for i in range(len(s)):
-            charS[s[i]] = 1 + charS.get(s[i],0)
-            charT[t[i]] = 1 + charT.get(t[i],0)
-        return charS == charT
-        
+        charS, charT = defaultdict(int), defaultdict(int)
+        for i, val in enumerate(s):
+            charS[val] = 1 + charS.get(val, 0)
+
+        for i, val in enumerate(t):
+            charT[val] = 1 + charT.get(val, 0)
+        return charS==charT
